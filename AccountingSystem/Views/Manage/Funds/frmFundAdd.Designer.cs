f2a8f69aa5d446866719a171da6f91fe6830d94e
Enrique Salomon <king92salomon@gmail.com>
2021-02-20 17:20:21 +0800

    Accept Button Fixed

--- AccountingSystem/Views/Manage/Funds/frmFundAdd.Designer.cs
+++ AccountingSystem/Views/Manage/Funds/frmFundAdd.Designer.cs
@@ -85,6 +85,7 @@ namespace AccountingSystem.Views.Manage.Funds
             // 
             // frmFundAdd
             // 
+            this.AcceptButton = this.btnSave;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
