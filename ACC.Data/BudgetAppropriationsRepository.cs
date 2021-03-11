d2c4882d554ce264b421948b4c1aefbc079fbde9
Janrey Ubane <rikimaru328@yahoo.com>
2021-03-11 15:46:32 +0800

    Created Budget Appropriations Class Repository

---
+++ ACC.Data/BudgetAppropriationsRepository.cs
@@ -0,0 +1,59 @@
+﻿using ACC.Domain.Interfaces;
+using ACC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ACC.Data
+{
+    public class BudgetAppropriationsRepository : IBudgetAppropriationsRepository
+    {
+        private MySqlGenericCommands mySqlGenericCommands;
+
+        public BudgetAppropriationsRepository(MySqlGenericCommands mySqlGenericCommands)
+        {
+            this.mySqlGenericCommands = mySqlGenericCommands;
+        }
+
+        public int CountRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(List<BudgetAppropriationsModel> entityList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(BudgetAppropriationsModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(BudgetAppropriationsModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
