cda7f89a7b2bb6f5e0c8318542b08431ced41a50
Janrey Ubane <rikimaru328@yahoo.com>
2021-03-11 15:37:26 +0800

    Created Budget Appropriations Model

---
+++ ACC.Domain/Models/BudgetAppropriationsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACC.Domain.Models
+{
+    public class BudgetAppropriationsModel
+    {
+        int ID { get; set; }
+        int FunctionProgramProjectId { get; set; }
+        int OthersFPPId { get; set; }
+        int AllotmentClassesId { get; set; }
+        int GeneralLedgerAccountsId { get; set; }
+        decimal amount { get; set; }
+    }
+}
