29a4c3c565bab5362d3bfb8e443702c623b94be1
Janrey Ubane <rikimaru328@yahoo.com>
2021-03-21 12:53:23 +0800

    Created Model for Allotment Realse Order Module

---
+++ ACC.Domain/Models/AllotmentReleaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACC.Domain.Models
+{
+    public class AllotmentReleaseModel
+    {
+        public int ID { get; set; }
+        public int BudgetAppropriationsID { get; set; }
+        public string ARONumber { get; set; }
+        public string Purpose { get; set; }
+        public DateTime DateIssued { get; set; }
+    }
+}
