9a9126cb474e300dd640db2400afbaedbd633883
Janrey Ubane <rikimaru328@yahoo.com>
2021-03-25 11:38:12 +0800

    Created Repository for Obligation Request Module

---
+++ ACC.Data/ObligationRequestRepository.cs
@@ -0,0 +1,52 @@
+﻿using ACC.Domain.Interfaces;
+using ACC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ACC.Data
+{
+    public class ObligationRequestRepository : IObligationRequestRepository
+    {
+        public int CountRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(List<ObligationRequestModel> entityList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(ObligationRequestModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(ObligationRequestModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
