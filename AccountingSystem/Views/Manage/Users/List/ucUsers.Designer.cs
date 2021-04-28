41fc268e481f54434de73192a01f225a43dc6d89
Cesar Ian Lesmoras <cilesmoras@gmail.com>
2021-04-28 11:08:19 +0800

    Updated the add & edit forms of Users

--- AccountingSystem/Views/Manage/Users/List/ucUsers.Designer.cs
+++ AccountingSystem/Views/Manage/Users/List/ucUsers.Designer.cs
@@ -59,18 +59,19 @@ namespace AccountingSystem.Views.Manage.Users.List
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 58);
+            this.label1.Location = new System.Drawing.Point(0, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.Size = new System.Drawing.Size(64, 15);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Firstname";
+            this.label1.Text = "First Name";
             // 
             // txtFirstname
             // 
-            this.txtFirstname.Location = new System.Drawing.Point(125, 55);
+            this.txtFirstname.Location = new System.Drawing.Point(82, 27);
+            this.txtFirstname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtFirstname.MaxLength = 45;
             this.txtFirstname.Name = "txtFirstname";
-            this.txtFirstname.Size = new System.Drawing.Size(345, 27);
+            this.txtFirstname.Size = new System.Drawing.Size(370, 23);
             this.txtFirstname.TabIndex = 2;
             this.txtFirstname.Validating += new System.ComponentModel.CancelEventHandler(this.txtFirstname_Validating);
             this.txtFirstname.Validated += new System.EventHandler(this.txtFirstname_Validated);
@@ -78,18 +79,19 @@ namespace AccountingSystem.Views.Manage.Users.List
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 91);
+            this.label2.Location = new System.Drawing.Point(0, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.Size = new System.Drawing.Size(76, 15);
             this.label2.TabIndex = 7;
             this.label2.Text = "Middle Initial";
             // 
             // txtMiddleInitial
             // 
-            this.txtMiddleInitial.Location = new System.Drawing.Point(125, 88);
+            this.txtMiddleInitial.Location = new System.Drawing.Point(82, 54);
+            this.txtMiddleInitial.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMiddleInitial.MaxLength = 3;
             this.txtMiddleInitial.Name = "txtMiddleInitial";
-            this.txtMiddleInitial.Size = new System.Drawing.Size(345, 27);
+            this.txtMiddleInitial.Size = new System.Drawing.Size(370, 23);
             this.txtMiddleInitial.TabIndex = 3;
             this.txtMiddleInitial.Validating += new System.ComponentModel.CancelEventHandler(this.txtMiddleInitial_Validating);
             this.txtMiddleInitial.Validated += new System.EventHandler(this.txtMiddleInitial_Validated);
@@ -97,18 +99,19 @@ namespace AccountingSystem.Views.Manage.Users.List
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 124);
+            this.label3.Location = new System.Drawing.Point(0, 84);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.Size = new System.Drawing.Size(63, 15);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Lastname";
+            this.label3.Text = "Last Name";
             // 
             // txtLastname
             // 
-            this.txtLastname.Location = new System.Drawing.Point(125, 121);
+            this.txtLastname.Location = new System.Drawing.Point(82, 81);
+            this.txtLastname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtLastname.MaxLength = 45;
             this.txtLastname.Name = "txtLastname";
-            this.txtLastname.Size = new System.Drawing.Size(345, 27);
+            this.txtLastname.Size = new System.Drawing.Size(370, 23);
             this.txtLastname.TabIndex = 4;
             this.txtLastname.Validating += new System.ComponentModel.CancelEventHandler(this.txtLastname_Validating);
             this.txtLastname.Validated += new System.EventHandler(this.txtLastname_Validated);
@@ -116,18 +119,19 @@ namespace AccountingSystem.Views.Manage.Users.List
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 157);
+            this.label4.Location = new System.Drawing.Point(0, 111);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.Size = new System.Drawing.Size(60, 15);
             this.label4.TabIndex = 11;
             this.label4.Text = "Username";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(125, 154);
+            this.txtUsername.Location = new System.Drawing.Point(82, 108);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtUsername.MaxLength = 45;
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(345, 27);
+            this.txtUsername.Size = new System.Drawing.Size(370, 23);
             this.txtUsername.TabIndex = 5;
             this.txtUsername.Validating += new System.ComponentModel.CancelEventHandler(this.txtUsername_Validating);
             this.txtUsername.Validated += new System.EventHandler(this.txtUsername_Validated);
@@ -135,28 +139,29 @@ namespace AccountingSystem.Views.Manage.Users.List
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 190);
+            this.label5.Location = new System.Drawing.Point(0, 138);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.Size = new System.Drawing.Size(57, 15);
             this.label5.TabIndex = 13;
             this.label5.Text = "Password";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(125, 187);
+            this.txtPassword.Location = new System.Drawing.Point(82, 135);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPassword.MaxLength = 60;
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(345, 27);
+            this.txtPassword.PasswordChar = '•';
+            this.txtPassword.Size = new System.Drawing.Size(370, 23);
             this.txtPassword.TabIndex = 6;
             this.txtPassword.Validating += new System.ComponentModel.CancelEventHandler(this.txtPassword_Validating);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 24);
+            this.label6.Location = new System.Drawing.Point(0, 3);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(39, 20);
+            this.label6.Size = new System.Drawing.Size(30, 15);
             this.label6.TabIndex = 15;
             this.label6.Text = "Role";
             // 
@@ -187,16 +192,17 @@ namespace AccountingSystem.Views.Manage.Users.List
             // cmbRoles
             // 
             this.cmbRoles.FormattingEnabled = true;
-            this.cmbRoles.Location = new System.Drawing.Point(125, 21);
+            this.cmbRoles.Location = new System.Drawing.Point(82, 0);
+            this.cmbRoles.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbRoles.Name = "cmbRoles";
-            this.cmbRoles.Size = new System.Drawing.Size(345, 28);
+            this.cmbRoles.Size = new System.Drawing.Size(370, 23);
             this.cmbRoles.TabIndex = 1;
             this.cmbRoles.Validating += new System.ComponentModel.CancelEventHandler(this.cmbRoles_Validating);
             this.cmbRoles.Validated += new System.EventHandler(this.cmbRoles_Validated);
             // 
             // ucUsers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.cmbRoles);
             this.Controls.Add(this.label6);
@@ -210,8 +216,9 @@ namespace AccountingSystem.Views.Manage.Users.List
             this.Controls.Add(this.txtMiddleInitial);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtFirstname);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ucUsers";
-            this.Size = new System.Drawing.Size(513, 237);
+            this.Size = new System.Drawing.Size(479, 171);
             this.Load += new System.EventHandler(this.ucUsers_Load);
             ((System.ComponentModel.ISupportInitialize)(this.epRole)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.epFirstName)).EndInit();
