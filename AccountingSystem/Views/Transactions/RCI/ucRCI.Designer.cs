17770fa5ae411d698a3d96991e82259202886c11
zermithdog <drlbenzie@yahoo.com>
2021-05-04 13:49:51 +0800

    check-issued report update and rci form update

--- AccountingSystem/Views/Transactions/RCI/ucRCI.Designer.cs
+++ AccountingSystem/Views/Transactions/RCI/ucRCI.Designer.cs
@@ -295,11 +295,11 @@ namespace AccountingSystem.Views.Transactions.RCI
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(375, 359);
+            this.label12.Location = new System.Drawing.Point(375, 354);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(97, 20);
+            this.label12.Size = new System.Drawing.Size(69, 20);
             this.label12.TabIndex = 25;
-            this.label12.Text = "Net Amount :";
+            this.label12.Text = "Amount :";
             // 
             // errorProvider
             // 
