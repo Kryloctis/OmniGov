24f737fb8ef527acaf15a690ff86bf3fda1e527d
Cesar Ian Lesmoras <cilesmoras@gmail.com>
2021-05-16 17:30:29 +0800

    Created model, interface and class repository of Cash Disbursement Journal

---
+++ ACC.Data/CashDisbursementsJournalRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ACC.Domain.Models;
+using ACC.Domain.Interfaces;
+using System.Data;
+
+namespace ACC.Data
+{
+    class CashDisbursementsJournalRepository : ICashDisbursementsJournalRepository
+    {
+        public int CountRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(List<CashDisbursementsJournalModel> entityList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(CashDisbursementsJournalModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CashDisbursementsJournalModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ACC.Domain/Models/CashDisbursementsJournalModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACC.Domain.Models
+{
+    public class CashDisbursementsJournalModel
+    {
+        public int Id { get; set; }
+        public int JevId { get; set; }
+        public int DisbursingOfficerId { get; set; }
+        public string DVNo { get; set; }
+        public DateTime DatePaid { get; set; }
+    }
+}
