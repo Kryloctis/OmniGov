06b40d9b0b5bc73f10a2df005e3b486579477a23
Janrey Ubane <rikimaru328@yahoo.com>
2021-05-17 09:43:14 +0800

    Added Obligation Request Datagrid View Method

--- AccountingSystem/HelperLoadRecords.cs
+++ AccountingSystem/HelperLoadRecords.cs
@@ -1021,5 +1021,36 @@ namespace AccountingSystem
 
         #endregion Supplemental Appropriations
 
+        #region Obligation Request
+
+        internal static void ObligationRequestDatagridView(DataTable dataTable, DataGridView dataGridView)
+        {
+            try
+            {
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    dataGridView.Rows.Add(new object[] {
+                            dataRow["obligation_request_id"],
+                            dataRow["obligation_no"],
+                            dataRow["account_code"],
+                            dataRow["gen_ledger_acc_name"],
+                            dataRow["date_requested"],
+                            dataRow["obligation_amount"],
+                            dataRow["created_at"],
+                            dataRow["updated_at"],
+                    });
+                }
+
+
+                dataGridView.ClearSelection();
+
+            }
+            catch (Exception ex)
+            {
+                Helper.MessageBoxError(ex.Message);
+            }
+        }
+
+        #endregion Obligation Request
     }
 }
