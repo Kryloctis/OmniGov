cf53e98f928c41beb64b445ed061ecbecf9b9785
Janrey Ubane <rikimaru328@yahoo.com>
2021-05-18 10:43:27 +0800

    Created Model for Obligation Request Module

---
+++ ACC.Domain/Models/ObligationRequestModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACC.Domain.Models
+{
+    public class ObligationRequestModule
+    {
+        public int Id { get; set; }
+        public int FundId { get; set; }
+        public int FPPId { get; set; }
+        public int? OtherFPPId { get; set; }
+        public int AllotmentClassId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string ObligationNo{ get; set; }
+        public string Payee{ get; set; }
+        public string Explanation{ get; set; }
+        public string ReferenceNo { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
