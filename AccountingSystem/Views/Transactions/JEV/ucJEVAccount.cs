0159e2aa412f1ed8837912cbe2cd92718b12ccc2
Janrey Ubane <rikimaru328@yahoo.com>
2021-06-29 11:18:01 +0800

    (Done) Load subsidiary combobox upon account selected value changed

--- AccountingSystem/Views/Transactions/JEV/ucJEVAccount.cs
+++ AccountingSystem/Views/Transactions/JEV/ucJEVAccount.cs
@@ -18,6 +18,61 @@ namespace AccountingSystem.Views.Transactions.JEV
             cmbAccount.DropDownHeight = 200;
         }
 
+
+        internal string GetFormErrors()
+        {
+            var errorArray = new string[3];
+            errorArray[0] = epFPP.GetError(cmbFPP);
+            errorArray[1] = epAccount.GetError(cmbAccount);
+            errorArray[2] = epAmount.GetError(nudAmount);
+
+            IError _errors = Factory.CreateErrors(errorArray);
+            return _errors.GenerateErrorMessage();
+
+        }
+
+        internal void ResetForm()
+        {
+            nudAmount.Value = 0;
+
+        }
+
+        internal void LoadSubsidiary()
+        {
+            try
+            {
+                ushort generalLedgerId = Convert.ToUInt16(cmbAccount.SelectedValue);
+                DataTable dtSubsidiary = Factory.SubsidiaryLedgerAccountsRepository().GetRecordsByFundAndGeneralLedger(fundId, generalLedgerId);
+
+                HelperLoadRecords.SubsidiaryLedgerComboBox(dtSubsidiary, cmbSubsidiary, "sub_name", "id");
+
+                btnSubsidiaryLedger.Enabled = true;
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
+        }
+
+        private void cmbAccount_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnSubsidiaryLedger_Click(object sender, EventArgs e)
+        {
+            ushort accountId = Convert.ToUInt16(cmbAccount.SelectedValue);
+            _ = new frmSubsidiary(accountId).ShowDialog();
+        }
+
+        private void ucJEVAccount_Load(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                // validate if it has permission
+                if (!Helper.HasPermission("Manage Subsidiary Ledger Account"))
+                    btnSubsidiaryLedger.Visible = false;
+            }
+        }
+
+
         //FPP
         private DataTable DataTableFPP()
         {
@@ -79,40 +134,42 @@ namespace AccountingSystem.Views.Transactions.JEV
             }
         }
 
-        internal string GetFormErrors()
-        {
-            var errorArray = new string[3];
-            errorArray[0] = epFPP.GetError(cmbFPP);
-            errorArray[1] = epAccount.GetError(cmbAccount);
-            errorArray[2] = epAmount.GetError(nudAmount);
 
-            IError _errors = Factory.CreateErrors(errorArray);
-            return _errors.GenerateErrorMessage();
 
-        }
+        //ACCOUNT
 
-        internal void ResetForm()
-        {
-            nudAmount.Value = 0;
-
-        }
-
-        internal void LoadSubsidiary(ushort generalLedgerId)
+        private void LoadAccounts() 
         {
             try
             {
-                DataTable dtSubsidiary = Factory.SubsidiaryLedgerAccountsRepository().GetRecordsByFundAndGeneralLedger(fundId, generalLedgerId);
+                Cursor.Current = Cursors.Default;
 
-                HelperLoadRecords.SubsidiaryLedgerComboBox(dtSubsidiary, cmbSubsidiary, "sub_name", "id");
+                DataTable dtAccounts = Factory.GeneralLedgerAccountsRepository().GetViewRecordsBySearch(cmbAccount.Text.Trim());
+
+                if (dtAccounts.Rows.Count == 0 || string.IsNullOrWhiteSpace(cmbAccount.Text.Trim())) return;
+
+                var accountDict = new Dictionary<int, string>();
+                foreach (DataRow item in dtAccounts.Rows)
+                {
+                    int accountId = Convert.ToInt32(item["general_ledger_accounts_id"]);
+                    string accountName = $"{item["account_code"]} - {item["ledger_name"]}";
+
+                    accountDict.Add(accountId, accountName);
+                }
+
+                cmbAccount.DataSource = new BindingSource(accountDict, null);
+                cmbAccount.DisplayMember = "value";
+                cmbAccount.ValueMember = "key";
+                cmbAccount.DroppedDown = true;
+                btnSubsidiaryLedger.Enabled = true;
+                cmbAccount.SelectedValueChanged += new EventHandler(cmbAccount_SelectedValueChanged);
+
+                Helper.ClearErrorComboBox(epAccount, cmbAccount);
             }
-            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
-        }
-
-        private void cmbAccount_SelectionChangeCommitted(object sender, EventArgs e)
-        {
-            ushort generalLedgerId = Convert.ToUInt16(cmbAccount.SelectedValue);
-            LoadSubsidiary(generalLedgerId);
-            btnSubsidiaryLedger.Enabled = true;
+            catch (Exception ex)
+            {
+                Helper.MessageBoxError(ex.Message);
+            }
         }
 
         private void cmbAccount_KeyDown(object sender, KeyEventArgs e)
@@ -121,54 +178,15 @@ namespace AccountingSystem.Views.Transactions.JEV
 
             if (e.KeyCode == Keys.F1)
             {
-                try
-                {
-                    Cursor.Current = Cursors.Default;
-
-                    DataTable dtAccounts = Factory.GeneralLedgerAccountsRepository().GetViewRecordsBySearch(cmbAccount.Text.Trim());
-
-                    if (dtAccounts.Rows.Count == 0 || string.IsNullOrWhiteSpace(cmbAccount.Text.Trim())) return;
-
-                    var accountDict = new Dictionary<int, string>();
-                    foreach (DataRow item in dtAccounts.Rows)
-                    {
-                        int accountId = Convert.ToInt32(item["general_ledger_accounts_id"]);
-                        string accountName = $"{item["account_code"]} - {item["ledger_name"]}";
-
-                        accountDict.Add(accountId, accountName);
-                    }
-
-                    cmbAccount.DataSource = new BindingSource(accountDict, null);
-                    cmbAccount.DisplayMember = "value";
-                    cmbAccount.ValueMember = "key";
-                    cmbAccount.DroppedDown = true;
-                    btnSubsidiaryLedger.Enabled = true;
-
-                    Helper.ClearErrorComboBox(epAccount, cmbAccount);
-                }
-                catch (Exception ex)
-                {
-                    Helper.MessageBoxError(ex.Message);
-                }
+                LoadAccounts();
+                LoadSubsidiary();
             }
         }
 
-        private void btnSubsidiaryLedger_Click(object sender, EventArgs e)
+        private void cmbAccount_SelectedValueChanged(object sender, EventArgs e)
         {
-            ushort accountId = Convert.ToUInt16(cmbAccount.SelectedValue);
-            _ = new frmSubsidiary(accountId).ShowDialog();
+            LoadSubsidiary();
         }
-
-        private void ucJEVAccount_Load(object sender, EventArgs e)
-        {
-            if (!DesignMode)
-            {
-                // validate if it has permission
-                if (!Helper.HasPermission("Manage Subsidiary Ledger Account"))
-                    btnSubsidiaryLedger.Visible = false;
-            }
-        }
-
 
 
         //VALIDATIONS
