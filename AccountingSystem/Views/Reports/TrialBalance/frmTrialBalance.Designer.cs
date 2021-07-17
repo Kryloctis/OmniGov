287b365e07ff7b85a573c450fa44dbe5981140c5
archie201998 <px1998.ass@gmail.com>
2021-07-17 14:38:42 +0800

    Create form for pre trial balance

---
+++ AccountingSystem/Views/Reports/TrialBalance/frmTrialBalance.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace AccountingSystem.Views.Reports.TrialBalance
+{
+    partial class frmTrialBalance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chkbxSpecialAccounts = new System.Windows.Forms.CheckBox();
+            this.dtAsOf = new System.Windows.Forms.DateTimePicker();
+            this.cmbxFund = new System.Windows.Forms.ComboBox();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelReport = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.chkbxSpecialAccounts);
+            this.panel1.Controls.Add(this.dtAsOf);
+            this.panel1.Controls.Add(this.cmbxFund);
+            this.panel1.Controls.Add(this.btnRetrieve);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(915, 47);
+            this.panel1.TabIndex = 2;
+            // 
+            // chkbxSpecialAccounts
+            // 
+            this.chkbxSpecialAccounts.AutoSize = true;
+            this.chkbxSpecialAccounts.Location = new System.Drawing.Point(567, 14);
+            this.chkbxSpecialAccounts.Name = "chkbxSpecialAccounts";
+            this.chkbxSpecialAccounts.Size = new System.Drawing.Size(116, 19);
+            this.chkbxSpecialAccounts.TabIndex = 7;
+            this.chkbxSpecialAccounts.Text = "Special Accounts";
+            this.chkbxSpecialAccounts.UseVisualStyleBackColor = true;
+            // 
+            // dtAsOf
+            // 
+            this.dtAsOf.Location = new System.Drawing.Point(333, 11);
+            this.dtAsOf.Margin = new System.Windows.Forms.Padding(3, 3, 20, 3);
+            this.dtAsOf.Name = "dtAsOf";
+            this.dtAsOf.Size = new System.Drawing.Size(211, 23);
+            this.dtAsOf.TabIndex = 6;
+            // 
+            // cmbxFund
+            // 
+            this.cmbxFund.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbxFund.FormattingEnabled = true;
+            this.cmbxFund.Location = new System.Drawing.Point(52, 12);
+            this.cmbxFund.Name = "cmbxFund";
+            this.cmbxFund.Size = new System.Drawing.Size(218, 23);
+            this.cmbxFund.TabIndex = 5;
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.Location = new System.Drawing.Point(706, 12);
+            this.btnRetrieve.Margin = new System.Windows.Forms.Padding(20, 3, 3, 3);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(92, 23);
+            this.btnRetrieve.TabIndex = 4;
+            this.btnRetrieve.Text = "Retrieve";
+            this.btnRetrieve.UseVisualStyleBackColor = true;
+            this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(20, 0, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "As of";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fund";
+            // 
+            // panelReport
+            // 
+            this.panelReport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelReport.Location = new System.Drawing.Point(0, 47);
+            this.panelReport.Name = "panelReport";
+            this.panelReport.Size = new System.Drawing.Size(915, 638);
+            this.panelReport.TabIndex = 3;
+            // 
+            // frmTrialBalance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 685);
+            this.Controls.Add(this.panelReport);
+            this.Controls.Add(this.panel1);
+            this.MinimizeBox = false;
+            this.Name = "frmTrialBalance";
+            this.Text = "Pre Closing Trial Balance";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkbxSpecialAccounts;
+        internal System.Windows.Forms.DateTimePicker dtAsOf;
+        internal System.Windows.Forms.ComboBox cmbxFund;
+        internal System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Panel panelReport;
+    }
+}
---
+++ AccountingSystem/Views/Reports/TrialBalance/frmTrialBalance.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Reports.TrialBalance
+{
+    public partial class frmTrialBalance : Form
+    {
+
+        private readonly ReportViewer reportViewer;
+
+        public frmTrialBalance()
+        {
+            InitializeComponent();
+            reportViewer = new ReportViewer();
+            reportViewer.Dock = DockStyle.Fill;
+            panel1.Controls.Add(reportViewer);
+        }
+
+        private void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            LoadReport(reportViewer.LocalReport);
+            reportViewer.SetDisplayMode(DisplayMode.PrintLayout);
+            reportViewer.ZoomMode = ZoomMode.Percent;
+            reportViewer.ZoomPercent = 100;
+            reportViewer.RefreshReport();
+        }
+
+        private void LoadReport(LocalReport report)
+        {
+            try
+            {
+
+                var lguDict = Helper.LGUDetails();
+                report.ReportPath = $"{Application.StartupPath}\\Reports\\pre-trial-balance.rdlc";
+                report.DataSources.Clear();
+
+                report.DataSources.Add(new ReportDataSource("dsPreTrialBalance", DataTablePreTrialBalance()));
+
+                //var parameters = new[] {
+                //    new ReportParameter("paramLGUName", lguDict["lgu_name"]),
+                //    new ReportParameter("paramFund", fundName),
+                //    new ReportParameter("paramAccountName", generalLedgerDict["ledger_name"]),
+                //    new ReportParameter("paramAccountCode", generalLedgerDict["account_code"]),
+                //    new ReportParameter("paramBalanceDate", balanceDate),
+                //    new ReportParameter("paramBalanceDebit", balanceDebit),
+                //    new ReportParameter("paramBalanceCredit", balanceCredit),
+                //    new ReportParameter("paramBalance", balance)
+                //  };
+                //report.SetParameters(parameters);
+
+            }
+            catch (Exception ex)
+            {
+                Helper.MessageBoxError(ex.Message);
+            }
+        }
+
+        private DataTable DataTablePreTrialBalance()
+        {
+
+
+            var dtPreTrialBalance = new dsLFS.dtPreTrialBalanceDataTable();
+            var dtPreTrialBalanceFromDB = Factory.GeneralLedgerAccountsRepository().GetAllViewRecords();
+
+            foreach (DataRow item in dtPreTrialBalanceFromDB.Rows)
+            {
+
+                DataRow row = dtPreTrialBalance.NewRow();
+                row["account_title"] = item["ledger_name"];
+                row["account_code"] = item["account_code"];
+
+
+                var beginningBalanceRepository = Factory.BeginningBalancesRepository();
+                decimal generalLedgerBalance = beginningBalanceRepository.GetSumBalanceByGeneralLedgerId(1, (ushort)item["ïd"], 2021);
+                var beginningBalanceDict = beginningBalanceRepository.GetRecordByFundsAndGeneralLedgerID(1, (ushort)item["ïd"], 2021);
+
+                string debitCreditType = string.Empty;
+                debitCreditType = HelperLoadRecords.ValidateDebitOrCreditType(beginningBalanceDict, debitCreditType);
+
+
+                if (debitCreditType == "Debit")
+                    row["debit"] = generalLedgerBalance;
+                else
+                    row["credit"] = generalLedgerBalance;
+
+                dtPreTrialBalance.Rows.Add(row);
+            }
+
+            return dtPreTrialBalance;
+        }
+    }
+}
