6ab45ddd7b6518bda7097b7dc506009251722479
archie201998 <px1998.ass@gmail.com>
2021-08-12 13:57:28 +0800

    Add accept button to JEV report.

--- AccountingSystem/Views/Reports/JEV/frmJEVReport.Designer.cs
+++ AccountingSystem/Views/Reports/JEV/frmJEVReport.Designer.cs
@@ -116,6 +116,7 @@ namespace AccountingSystem.Views.Reports.JEV
             // 
             // frmJEVReport
             // 
+            this.AcceptButton = this.btnRetrieve;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
