0ad5e86a734bf12feb05ace87fbb62ae0244e688
Janrey Ubane <rikimaru328@yahoo.com>
2021-08-31 12:37:37 +0800

    Fixed Dashboard error

--- AccountingSystem/Views/Dashboard/BudgetDashboard/UcBudgetDashboard.cs
+++ AccountingSystem/Views/Dashboard/BudgetDashboard/UcBudgetDashboard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccountingSystem.Views.Dashboard.BudgetDashboard.BudgetSummary;
+using System;
 using System.Windows.Forms;
 
 namespace AccountingSystem.Views.Dashboard
@@ -6,13 +7,21 @@ namespace AccountingSystem.Views.Dashboard
     public partial class ucBudgetDashboard : UserControl
     {
 
+        private ucBudgetSummary uc;
+
         public ucBudgetDashboard()
         {
             InitializeComponent();
+            uc = ucBudgetSummary1;
         }
 
         private void UcBudgetDashboard_Load(object sender, EventArgs e)
         {
+            if (!DesignMode)
+            {
+                uc.LoadBudgetDashboardComboboxes();
+                uc.LoadBudgetDashboardContents();
+            }
         }
 
         private void chkbxDetailed_CheckedChanged(object sender, EventArgs e)
