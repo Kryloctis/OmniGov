11deee48fef67f0c308c5613979a9b5ce7fb02c3
Janrey Ubane <rikimaru328@yahoo.com>
2021-10-19 23:37:01 +0800

    Fixed Trial Balance Reports format.

--- AccountingSystem/Views/Reports/TrialBalance/ucPreClosingTrialBalance.cs
+++ AccountingSystem/Views/Reports/TrialBalance/ucPreClosingTrialBalance.cs
@@ -98,7 +98,7 @@ namespace AccountingSystem.Views.Reports.TrialBalance
                 report.DataSources.Add(new ReportDataSource("dtTrialBalance", DataTablePreTrialBalance()));
 
                 var signatory = "MARY MAGDALYN T. REGANION, CPA";
-                var fundName = cmbFund.Text.ToUpper();
+                var fundName = cmbFund.Text;
                 var asOfDate = dtAsOf.Value.ToString("MMMM dd, yyyy");
 
                 var parameters = new[] {
