cbb93f565fd439c5d395312ca2ca9fba3b81f01c
zermithdog <drlbenzie@yahoo.com>
2021-11-15 10:43:03 +0800

    Report of Collections Menu Item and Collector Access

--- AccountingSystem/Views/Dashboard/TreasuryDashboard/ucTreasuryDashboard.cs
+++ AccountingSystem/Views/Dashboard/TreasuryDashboard/ucTreasuryDashboard.cs
@@ -14,6 +14,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AccountingSystem.Views.Dashboard.TreasuryDashboard.RCDSummary;
+using AccountingSystem.Views.Reports.PaymentCollection;
 
 namespace AccountingSystem.Views.Dashboard.TreasuryDashboard
 {
@@ -62,7 +63,19 @@ namespace AccountingSystem.Views.Dashboard.TreasuryDashboard
 
         private void ucTreasuryDashboard_Load(object sender, EventArgs e)
         {
+            ButtonAccess();
+        }
 
+        private void ButtonAccess()
+        {
+            var uRepository = Factory.UsersRepository();
+            bool is_collector = uRepository.GetUserRole(Helper.UserId) == "Collecting Officer" ? true : false;
+            btnReportCollections.Visible = is_collector;
+        }
+
+        private void btnReportCollections_Click(object sender, EventArgs e)
+        {
+            _ = new frmPCReport().ShowDialog();
         }
     }
 }
