e593cb724b6cc7c8ec4ded82382a488987bcf13f
archie201998 <px1998.ass@gmail.com>
2021-11-18 11:17:41 +0800

    created user control for collectors rcd.

---
+++ AccountingSystem/Views/Reports/CollectorsRCD/ucCollectorsRCD.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace AccountingSystem.Views.Reports.RCDCollector
+{
+    partial class ucCollectorsRCD
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dtdate = new System.Windows.Forms.DateTimePicker();
+            this.txtreport = new System.Windows.Forms.TextBox();
+            this.cmbcollector = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanelFunds = new System.Windows.Forms.FlowLayoutPanel();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvpayments = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvpayments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtdate
+            // 
+            this.dtdate.Location = new System.Drawing.Point(561, 76);
+            this.dtdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtdate.Name = "dtdate";
+            this.dtdate.Size = new System.Drawing.Size(225, 23);
+            this.dtdate.TabIndex = 19;
+            // 
+            // txtreport
+            // 
+            this.txtreport.Location = new System.Drawing.Point(79, 76);
+            this.txtreport.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtreport.MaxLength = 20;
+            this.txtreport.Name = "txtreport";
+            this.txtreport.Size = new System.Drawing.Size(171, 23);
+            this.txtreport.TabIndex = 17;
+            // 
+            // cmbcollector
+            // 
+            this.cmbcollector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbcollector.FormattingEnabled = true;
+            this.cmbcollector.Location = new System.Drawing.Point(333, 77);
+            this.cmbcollector.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbcollector.Name = "cmbcollector";
+            this.cmbcollector.Size = new System.Drawing.Size(171, 23);
+            this.cmbcollector.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(522, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Report No";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Collector";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.flowLayoutPanelFunds);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(907, 59);
+            this.groupBox1.TabIndex = 27;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Funds";
+            // 
+            // flowLayoutPanelFunds
+            // 
+            this.flowLayoutPanelFunds.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelFunds.Location = new System.Drawing.Point(3, 18);
+            this.flowLayoutPanelFunds.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanelFunds.Name = "flowLayoutPanelFunds";
+            this.flowLayoutPanelFunds.Size = new System.Drawing.Size(901, 39);
+            this.flowLayoutPanelFunds.TabIndex = 0;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(49, 482);
+            this.txttotal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.ReadOnly = true;
+            this.txttotal.Size = new System.Drawing.Size(229, 23);
+            this.txttotal.TabIndex = 26;
+            this.txttotal.Text = "0.00";
+            this.txttotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btndelete.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btndelete.Location = new System.Drawing.Point(751, 482);
+            this.btndelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(74, 23);
+            this.btndelete.TabIndex = 25;
+            this.btndelete.Text = "Delete";
+            this.btndelete.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnclear.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnclear.Location = new System.Drawing.Point(830, 482);
+            this.btnclear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(74, 23);
+            this.btnclear.TabIndex = 24;
+            this.btnclear.Text = "Clear";
+            this.btnclear.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnadd.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnadd.Location = new System.Drawing.Point(675, 482);
+            this.btnadd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(70, 23);
+            this.btnadd.TabIndex = 23;
+            this.btnadd.Text = "Add";
+            this.btnadd.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 486);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Total";
+            // 
+            // dgvpayments
+            // 
+            this.dgvpayments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvpayments.Location = new System.Drawing.Point(9, 107);
+            this.dgvpayments.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvpayments.Name = "dgvpayments";
+            this.dgvpayments.RowHeadersWidth = 51;
+            this.dgvpayments.RowTemplate.Height = 29;
+            this.dgvpayments.Size = new System.Drawing.Size(895, 370);
+            this.dgvpayments.TabIndex = 21;
+            // 
+            // ucRCDCollector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dtdate);
+            this.Controls.Add(this.txtreport);
+            this.Controls.Add(this.cmbcollector);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgvpayments);
+            this.Name = "ucRCDCollector";
+            this.Size = new System.Drawing.Size(913, 510);
+            this.Load += new System.EventHandler(this.ucRCDCollector_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvpayments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.DateTimePicker dtdate;
+        internal System.Windows.Forms.TextBox txtreport;
+        internal System.Windows.Forms.ComboBox cmbcollector;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.GroupBox groupBox1;
+        internal System.Windows.Forms.FlowLayoutPanel flowLayoutPanelFunds;
+        internal System.Windows.Forms.TextBox txttotal;
+        internal System.Windows.Forms.Button btndelete;
+        internal System.Windows.Forms.Button btnclear;
+        internal System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.DataGridView dgvpayments;
+    }
+}
---
+++ AccountingSystem/Views/Reports/CollectorsRCD/ucCollectorsRCD.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Reports.RCDCollector
+{
+    public partial class ucCollectorsRCD : UserControl
+    {
+        private byte fundId;
+
+        public ucCollectorsRCD()
+        {
+            InitializeComponent();
+        }
+
+        private void ucRCDCollector_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        internal void LoadFunds()
+        {
+            var funds = Factory.FundsRepository().GetRecords();
+
+            foreach (DataRow fund in funds.Rows)
+            {
+                var radFund = new RadioButton
+                {
+                    Text = fund["fund_name"].ToString(),
+                    Tag = fund["id"],
+                    AutoSize = true,
+                    Appearance = Appearance.Button,
+                    TextImageRelation = TextImageRelation.ImageBeforeText
+                };
+
+                // making general fund as default
+                if (fund["fund_name"].ToString() == "General Fund")
+                {
+                    radFund.Checked = true;
+                    fundId = Convert.ToByte(fund["id"]);
+                    if (radFund.Checked)
+                        radFund.Image = Properties.Resources.ok14px;
+                    else
+                        radFund.Image = null;
+                }
+
+
+                flowLayoutPanelFunds.Controls.Add(radFund);
+
+                radFund.Click += (s, e) => {
+                    var radFund = s as RadioButton;
+                    fundId = Convert.ToByte(radFund.Tag);
+                };
+                radFund.CheckedChanged += (s, e) => {
+                    var radFund = s as RadioButton;
+                    if (radFund.Checked)
+                        radFund.Image = Properties.Resources.ok14px;
+                    else
+                        radFund.Image = null;
+                };
+            }
+        }
+    }
+}
