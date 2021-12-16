f263797d9ca5171bd7e4632b875edbef1cfd5276
Janrey Ubane <rikimaru328@yahoo.com>
2021-12-17 00:32:56 +0800

    Created References Repository.

---
+++ ACC.Data/SignatoriesHasPreferencesRepository.cs
@@ -0,0 +1,50 @@
+﻿using ACC.Domain.Interfaces;
+using ACC.Domain.Models;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ACC.Data
+{
+    public class SignatoriesHasPreferencesRepository : ISignatoriesHasReferences
+    {
+        public int CountRecords()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(List<SignatoriesHasReferencesModel> entityList)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Insert(SignatoriesHasReferencesModel entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(SignatoriesHasReferencesModel entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
