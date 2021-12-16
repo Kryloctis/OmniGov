19076afef577f264c528ee56b2db8ce78b65efc0
Janrey Ubane <rikimaru328@yahoo.com>
2021-12-17 00:24:31 +0800

    Created Documents Model.

---
+++ ACC.Domain/Models/DocumentsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ACC.Domain.Models
+{
+    public class DocumentsModel
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Office { get; set; }
+    }
+}
