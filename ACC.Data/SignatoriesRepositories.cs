8a8e1e604d3439b4d1f80d620f91c5f1ceb286cf
Janrey Ubane <rikimaru328@yahoo.com>
2021-12-17 00:30:57 +0800

    Created Signatories Repository.

---
+++ ACC.Data/SignatoriesRepositories.cs
@@ -0,0 +1,51 @@
+﻿using ACC.Domain.Interfaces;
+using ACC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ACC.Data
+{
+    public class SignatoriesRepositories : ISignatories
+    {
+        public int CountRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(List<SignatoriesModel> entityList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(SignatoriesModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(SignatoriesModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
