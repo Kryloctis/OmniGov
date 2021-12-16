cfb5970cc8f04c36e6b44a352262c6070185591c
Janrey Ubane <rikimaru328@yahoo.com>
2021-12-17 00:37:46 +0800

    Created Repository for Documents.

---
+++ ACC.Data/DocumentsRepository.cs
@@ -0,0 +1,50 @@
+﻿using ACC.Domain.Interfaces;
+using ACC.Domain.Models;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ACC.Data
+{
+    public class DocumentsRepository : IDocuments
+    {
+        public int CountRecords()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(List<DocumentsModel> entityList)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Insert(DocumentsModel entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(DocumentsModel entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- ACC.Domain/Interfaces/IDocuments.cs
+++ ACC.Domain/Interfaces/IDocuments.cs
@@ -2,7 +2,7 @@
 
 namespace ACC.Domain.Interfaces
 {
-    interface IDocuments : IRepository<DocumentsModel>
+    public interface IDocuments : IRepository<DocumentsModel>
     {
     }
 }
