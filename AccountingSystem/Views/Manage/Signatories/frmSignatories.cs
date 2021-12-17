3d83da7b806bf22a09d213a8e6cbe2aded6eedff
Janrey Ubane <rikimaru328@yahoo.com>
2021-12-17 09:57:53 +0800

    Created user interface for signatories.

---
+++ AccountingSystem/Views/Manage/Signatories/frmSignatories.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Manage.Signatories
+{
+    public partial class frmSignatories : Form
+    {
+        public frmSignatories()
+        {
+            InitializeComponent();
+            Helper.LoadFormIcon(this);
+        }
+    }
+}
---
+++ AccountingSystem/Views/Manage/Signatories/ucSignatories.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Manage.Signatories
+{
+    public partial class ucSignatories : UserControl
+    {
+        public ucSignatories()
+        {
+            InitializeComponent();
+        }
+    }
+}
