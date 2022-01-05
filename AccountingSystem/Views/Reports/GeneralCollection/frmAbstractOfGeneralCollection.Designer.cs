560637406b649c46dfbbd1a4e9129ae9f255b574
archie201998 <px1998.ass@gmail.com>
2022-01-05 09:30:45 +0800

    Added back Abstract of General Collection (Missing file)

---
+++ AccountingSystem/Views/Reports/GeneralCollection/frmAbstractOfGeneralCollection.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace AccountingSystem.Views.Reports.GeneralCollection
+{
+    partial class frmAbstractOfGeneralCollection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpMonth = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtto = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpMonth
+            // 
+            this.dtpMonth.CustomFormat = "";
+            this.dtpMonth.Location = new System.Drawing.Point(64, 10);
+            this.dtpMonth.Name = "dtpMonth";
+            this.dtpMonth.Size = new System.Drawing.Size(258, 23);
+            this.dtpMonth.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "From:";
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.Location = new System.Drawing.Point(627, 8);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(75, 23);
+            this.btnRetrieve.TabIndex = 16;
+            this.btnRetrieve.Text = "Retrieve";
+            this.btnRetrieve.UseVisualStyleBackColor = true;
+            this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(13, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1038, 615);
+            this.panel1.TabIndex = 19;
+            // 
+            // dtto
+            // 
+            this.dtto.Location = new System.Drawing.Point(363, 8);
+            this.dtto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtto.Name = "dtto";
+            this.dtto.Size = new System.Drawing.Size(258, 23);
+            this.dtto.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "To:";
+            // 
+            // frmPCReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 562);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtto);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dtpMonth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRetrieve);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPCReport";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reports > Reports of General Collection";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmPCReport_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dtto;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ AccountingSystem/Views/Reports/GeneralCollection/frmAbstractOfGeneralCollection.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Reporting.WinForms;
+using SpreadsheetLight;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Reports.GeneralCollection
+{
+    public partial class frmAbstractOfGeneralCollection : Form
+    {
+        private readonly ReportViewer reportViewer = new ReportViewer();
+
+        public frmAbstractOfGeneralCollection()
+        {
+            InitializeComponent();
+            reportViewer.Dock = DockStyle.Fill;
+            panel1.Controls.Add(reportViewer);
+            Helper.LoadFormIcon(this);
+        }
+
+        private void frmPCReport_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private DataTable DataTablePC()
+        {
+            var dateYearMonth = String.Format("{0:MMMM}-{0:yyyy}", Convert.ToDateTime(dtpMonth.Value));
+            var dtPC = new dsLFS.dtPCDataTable();
+            var dt = Factory.PaymentCollectionRepository().GetRecordByLedger(dateYearMonth);
+            if(dt.Rows.Count > 0)
+            {
+                foreach (DataRow item in dt.Rows)
+                {
+                    DataRow row = dtPC.NewRow();
+                    row["rcdno"] = item["rcdno"];
+                    row["account_code"] = item["account_code"];
+                    row["subsidiary"] = item["subsidiary"];
+                    row["payee"] = item["payee"];
+                    row["acc_form_desc"] = item["accform"];
+                    row["ledger_name"] = item["ledger_name"];
+                    row["payment_date"] = item["payment_date"];
+                    row["receipt_no"] = item["receipt_no"];
+                    row["amount"] = item["amount"];
+                    row["collector"] = item["collector"];                    
+                    dtPC.Rows.Add(row);
+                }
+            }
+
+            return dtPC;
+        }
+
+        private DataTable DataTableGC(string from,string to)
+        {
+            
+            var dtPC = new dsLFS.dtPCDataTable();
+            var dt = Factory.GeneralCollectionsRepository().GetRecordByGC(from,to);
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow item in dt.Rows)
+                {
+                    DataRow row = dtPC.NewRow();
+                    row["rcdid"] = item["id"];
+                    row["rcdno"] = item["rcd_no"];
+                    row["reportno"] = item["report_no"];
+                    row["account_code"] = item["account_code"];
+                    row["subsidiary"] = item["subsidiary"];
+                    row["payee"] = item["payee"];
+                    row["acc_form_desc"] = item["accform"];
+                    row["ledger_name"] = item["ledger_name"];
+                    row["payment_date"] = item["payment_date"];
+                    row["receipt_no"] = item["receipt_no"];
+                    row["amount"] = item["amount"];
+                    row["collector"] = item["collector"];
+                    dtPC.Rows.Add(row);
+                }
+            }
+
+            return dtPC;
+        }
+
+
+        private void LoadReport(LocalReport report)
+        {
+            try
+            {
+                string from = String.Format("{0:yyyy-MM-dd}", dtpMonth.Value);
+                string to = String.Format("{0:yyyy-MM-dd}", dtto.Value);
+                var lguDetails = Helper.LGUDetails();
+                var signatory = "ENSIGN S. UBA";
+                var parameters = new[] {
+                            new ReportParameter("paramLGUName", lguDetails["lgu_name"]),
+                            new ReportParameter("paramSignatory", signatory)
+                    };
+                report.ReportPath = $"{Application.StartupPath}Reports\\payment-collection2.rdlc";
+                report.DataSources.Clear();
+                report.DataSources.Add(new ReportDataSource("dtPC", DataTableGC(from,to)));
+                report.SetParameters(parameters);
+                report.Refresh();
+
+
+            }
+            catch (Exception ex)
+            {
+                Helper.MessageBoxError(ex.Message);
+            }
+
+           
+        }
+
+        private void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            LoadReport(reportViewer.LocalReport);
+            reportViewer.SetDisplayMode(DisplayMode.PrintLayout);
+            reportViewer.ZoomMode = ZoomMode.Percent;
+            reportViewer.ZoomPercent = 100;
+            reportViewer.RefreshReport();
+        }
+
+   
+    }
+}
