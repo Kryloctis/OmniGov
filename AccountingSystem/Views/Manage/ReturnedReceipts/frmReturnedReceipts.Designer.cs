3ce1e2348b3da718a2ecaad34fcb4192cc787918
archie201998 <px1998.ass@gmail.com>
2022-01-10 11:01:41 +0800

    added form for returned receipts

---
+++ AccountingSystem/Views/Manage/ReturnedReceipts/frmReturnedReceipts.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace AccountingSystem.Views.Manage.ReturnedReceipts
+{
+    partial class frmReturnedReceipts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmReturnedReceipts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(735, 395);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmReturnedReceipts";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Returned Receipts";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
