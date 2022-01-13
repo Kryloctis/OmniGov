7fd2d79d48c7f5b6813aa30b30e09a91f739e31d
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-01-13 10:24:01 -1200

    Fixed validation of combobox bank.

--- AccountingSystem/Views/Reports/RCI/frmRCIReport.Designer.cs
+++ AccountingSystem/Views/Reports/RCI/frmRCIReport.Designer.cs
@@ -29,103 +29,123 @@ namespace AccountingSystem.Views.Reports.RCI
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.cmbBanks = new System.Windows.Forms.ComboBox();
             this.dtpMonth = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.btnRetrieve = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(991, 524);
+            this.panel1.TabIndex = 12;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.cmbBanks);
+            this.panel2.Controls.Add(this.dtpMonth);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnRetrieve);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(991, 38);
+            this.panel2.TabIndex = 13;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 17);
+            this.label2.Location = new System.Drawing.Point(12, 15);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 20);
-            this.label2.TabIndex = 17;
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 22;
             this.label2.Text = "Bank Account:";
             // 
             // cmbBanks
             // 
             this.cmbBanks.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbBanks.FormattingEnabled = true;
-            this.cmbBanks.Location = new System.Drawing.Point(126, 13);
-            this.cmbBanks.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbBanks.Location = new System.Drawing.Point(110, 12);
             this.cmbBanks.Name = "cmbBanks";
-            this.cmbBanks.Size = new System.Drawing.Size(209, 28);
-            this.cmbBanks.TabIndex = 16;
+            this.cmbBanks.Size = new System.Drawing.Size(183, 23);
+            this.cmbBanks.TabIndex = 21;
+            this.cmbBanks.Validating += new System.ComponentModel.CancelEventHandler(this.cmbBanks_Validating);
+            this.cmbBanks.Validated += new System.EventHandler(this.cmbBanks_Validated);
             // 
             // dtpMonth
             // 
             this.dtpMonth.CustomFormat = "MMMM-yyyy";
             this.dtpMonth.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpMonth.Location = new System.Drawing.Point(398, 12);
-            this.dtpMonth.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dtpMonth.Location = new System.Drawing.Point(373, 12);
             this.dtpMonth.Name = "dtpMonth";
-            this.dtpMonth.Size = new System.Drawing.Size(175, 27);
-            this.dtpMonth.TabIndex = 15;
+            this.dtpMonth.Size = new System.Drawing.Size(154, 23);
+            this.dtpMonth.TabIndex = 20;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(342, 17);
+            this.label1.Location = new System.Drawing.Point(324, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 20);
-            this.label1.TabIndex = 14;
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 19;
             this.label1.Text = "Month";
             // 
             // btnRetrieve
             // 
-            this.btnRetrieve.Location = new System.Drawing.Point(581, 12);
-            this.btnRetrieve.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnRetrieve.Location = new System.Drawing.Point(533, 12);
             this.btnRetrieve.Name = "btnRetrieve";
-            this.btnRetrieve.Size = new System.Drawing.Size(86, 31);
-            this.btnRetrieve.TabIndex = 13;
+            this.btnRetrieve.Size = new System.Drawing.Size(86, 23);
+            this.btnRetrieve.TabIndex = 18;
             this.btnRetrieve.Text = "Retrieve";
             this.btnRetrieve.UseVisualStyleBackColor = true;
             this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
             // 
-            // panel1
+            // errorProvider1
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Location = new System.Drawing.Point(18, 67);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1103, 688);
-            this.panel1.TabIndex = 12;
+            this.errorProvider1.ContainerControl = this;
             // 
             // frmRCIReport
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1133, 768);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.cmbBanks);
-            this.Controls.Add(this.dtpMonth);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnRetrieve);
+            this.ClientSize = new System.Drawing.Size(991, 562);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmRCIReport";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Report > Report of Checks Issued";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmRCIReport_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cmbBanks;
         private System.Windows.Forms.DateTimePicker dtpMonth;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnRetrieve;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- AccountingSystem/Views/Reports/RCI/frmRCIReport.cs
+++ AccountingSystem/Views/Reports/RCI/frmRCIReport.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.Reporting.WinForms;
+﻿using ACC.Domain.Interfaces;
+using Microsoft.Reporting.WinForms;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AccountingSystem.Views.Reports.RCI
@@ -28,6 +23,15 @@ namespace AccountingSystem.Views.Reports.RCI
             LoadBanks();
         }
 
+        internal string GetFormErrors()
+        {
+            var errorArray = new string[1];
+            errorArray[0] = errorProvider1.GetError(cmbBanks);
+
+            IError _errors = Factory.CreateErrors(errorArray);
+            return _errors.GenerateErrorMessage();
+        }
+
         private void LoadBanks()
         {
             cmbBanks.DataSource = Factory.BanksRepository().GetRecords();
@@ -43,11 +47,11 @@ namespace AccountingSystem.Views.Reports.RCI
 
             var dtRCI = new dsLFS.dtRCIDataTable();
             var dt = Factory.RCIRepository().GetRecordsByAccountId(bankId, dateYearMonth);
-            if(dt.Rows.Count > 0)
+            if (dt.Rows.Count > 0)
             {
-                foreach(DataRow item in dt.Rows)
+                foreach (DataRow item in dt.Rows)
                 {
-                    
+
                     DataRow row = dtRCI.NewRow();
                     row["account_no"] = item["account_no"];
                     row["bank_name"] = item["bank_name"];
@@ -69,9 +73,14 @@ namespace AccountingSystem.Views.Reports.RCI
 
         private void LoadReport(LocalReport report)
         {
-           
             try
             {
+                if (!ValidateChildren())
+                {
+                    Helper.MessageBoxError(GetFormErrors());
+                    return;
+                }
+
                 var lguDetails = Helper.LGUDetails();
                 var signatory = "MARY MAGDALYN T. REGANION, CPA";
 
@@ -82,7 +91,8 @@ namespace AccountingSystem.Views.Reports.RCI
                     new ReportParameter("paramLGUName", lguDetails["lgu_name"]),
                     new ReportParameter("paramBankaccount", bankDetails),
                     new ReportParameter("paramMonth", dtpMonth.Value.ToString()),
-                    new ReportParameter("paramSignatory", signatory)
+                    new ReportParameter("paramMunicipalTreasurerSignatory", signatory),
+                    new ReportParameter("paramAdministrativeOfficerSignatory", string.Empty)
                 };
 
                 report.ReportPath = $"{Application.StartupPath}Reports\\check-issued.rdlc";
@@ -90,6 +100,10 @@ namespace AccountingSystem.Views.Reports.RCI
 
                 report.DataSources.Add(new ReportDataSource("dtRCI", DataTableRCI()));
                 report.SetParameters(parameters);
+                reportViewer.SetDisplayMode(DisplayMode.PrintLayout);
+                reportViewer.ZoomMode = ZoomMode.Percent;
+                reportViewer.ZoomPercent = 100;
+                reportViewer.RefreshReport();
 
             }
             catch (Exception ex)
@@ -100,14 +114,17 @@ namespace AccountingSystem.Views.Reports.RCI
 
         private void btnRetrieve_Click(object sender, EventArgs e)
         {
-            if (cmbBanks.SelectedIndex == -1)
-                Helper.MessageBoxError("Please select Bank!");
-              
             LoadReport(reportViewer.LocalReport);
-            reportViewer.SetDisplayMode(DisplayMode.PrintLayout);
-            reportViewer.ZoomMode = ZoomMode.Percent;
-            reportViewer.ZoomPercent = 100;
-            reportViewer.RefreshReport();
+        }
+
+        private void cmbBanks_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = Helper.ShowErrorComboBoxEmpty(errorProvider1, cmbBanks, "Bank");
+        }
+
+        private void cmbBanks_Validated(object sender, EventArgs e)
+        {
+            Helper.ClearErrorComboBox(errorProvider1, cmbBanks);
         }
     }
 }
