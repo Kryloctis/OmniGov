fb06a47f2d29de701864fe3e0744d63c55d14ab5
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-01-13 09:15:58 -1200

    Applied fixed prefix and suffix to reports.

--- AccountingSystem/Views/Reports/JEV/frmJEVReport.cs
+++ AccountingSystem/Views/Reports/JEV/frmJEVReport.cs
@@ -118,7 +118,15 @@ namespace AccountingSystem.Views.Reports.JEV
                 {
                     if (dictSignatory.Count > 0)
                     {
-                        signatory = dictSignatory["signatories_full_name"];
+                        string prefix = dictSignatory["signatories_prefix"].ToString();
+                        string firstName = dictSignatory["signatories_first_name"].ToString();
+                        char middleInitial = Convert.ToChar(dictSignatory["signatories_middle_initial"]);
+                        string lastName = dictSignatory["signatories_last_name"].ToString();
+                        string suffix = dictSignatory["signatories_suffix"].ToString();
+
+                        string signatoryName = $"{(string.IsNullOrEmpty(prefix) ? string.Empty : $"{prefix}.")} {firstName} {middleInitial}. {lastName}{(string.IsNullOrEmpty(suffix) ? string.Empty : $", {suffix}")}";
+
+                        signatory = signatoryName;
                         signatory_title = dictSignatory["signatories_title"];
                     }
                 }
