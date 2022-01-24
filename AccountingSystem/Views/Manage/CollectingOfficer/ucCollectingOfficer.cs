7377ff91627f67c5849ab38243093bb10f4df380
archie201998 <px1998.ass@gmail.com>
2022-01-24 11:43:38 +0800

    Remove unuse classes in collecting officer usercontrol.

--- AccountingSystem/Views/Manage/CollectingOfficer/ucCollectingOfficer.cs
+++ AccountingSystem/Views/Manage/CollectingOfficer/ucCollectingOfficer.cs
@@ -1,13 +1,7 @@
 ﻿using ACC.Domain.Interfaces;
 using AccountingSystem.Views.Manage.LinkUser;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AccountingSystem.Views.Manage.CollectingOfficer
