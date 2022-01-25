0f0519e56682cfc3d17b44ac806156a69b50960f
archie201998 <px1998.ass@gmail.com>
2022-01-25 14:19:15 +0800

    Added group box in obligation form.

--- AccountingSystem/Views/Transactions/CheckIssuance/Obligations/frmObligations.Designer.cs
+++ AccountingSystem/Views/Transactions/CheckIssuance/Obligations/frmObligations.Designer.cs
@@ -33,8 +33,10 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Obligations
             this.btnAdd = new System.Windows.Forms.Button();
             this.txtObno = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,7 +48,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Obligations
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.groupBox1.Location = new System.Drawing.Point(4, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(392, 67);
+            this.groupBox1.Size = new System.Drawing.Size(370, 67);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
@@ -56,7 +58,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Obligations
             this.btnAdd.Enabled = false;
             this.btnAdd.Location = new System.Drawing.Point(300, 38);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(69, 23);
+            this.btnAdd.Size = new System.Drawing.Size(64, 23);
             this.btnAdd.TabIndex = 19;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -67,7 +69,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Obligations
             this.txtObno.Location = new System.Drawing.Point(97, 13);
             this.txtObno.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtObno.Name = "txtObno";
-            this.txtObno.Size = new System.Drawing.Size(272, 21);
+            this.txtObno.Size = new System.Drawing.Size(267, 21);
             this.txtObno.TabIndex = 18;
             // 
             // label6
@@ -79,22 +81,35 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Obligations
             this.label6.TabIndex = 21;
             this.label6.Text = "Obligation No.";
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(4, 73);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(370, 205);
+            this.groupBox2.TabIndex = 33;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "List";
+            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(4, 73);
+            this.dataGridView1.Location = new System.Drawing.Point(10, 21);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(392, 137);
-            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Size = new System.Drawing.Size(351, 176);
+            this.dataGridView1.TabIndex = 0;
             // 
             // frmObligations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(401, 212);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(380, 282);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
@@ -106,6 +121,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Obligations
             this.Text = "Obligations No/s";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -117,6 +133,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Obligations
         private System.Windows.Forms.Button btnAdd;
         internal System.Windows.Forms.ComboBox txtObno;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
