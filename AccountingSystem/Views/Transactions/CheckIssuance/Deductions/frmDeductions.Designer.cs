17a49345127b04867c944bd098ae3a9c73f20475
archie201998 <px1998.ass@gmail.com>
2022-01-26 10:54:20 +0800

    rename deduction field name

--- AccountingSystem/Views/Transactions/CheckIssuance/Deductions/frmDeductions.Designer.cs
+++ AccountingSystem/Views/Transactions/CheckIssuance/Deductions/frmDeductions.Designer.cs
@@ -32,15 +32,15 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.nudAmount = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.txtnature = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgDeductions = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDeductions)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -49,7 +49,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.btnAdd);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.nudAmount);
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.groupBox1.Location = new System.Drawing.Point(7, 3);
             this.groupBox1.Name = "groupBox1";
@@ -62,7 +62,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             // 
             this.btnAdd.Enabled = false;
             this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnAdd.Location = new System.Drawing.Point(80, 89);
+            this.btnAdd.Location = new System.Drawing.Point(281, 89);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(59, 23);
             this.btnAdd.TabIndex = 19;
@@ -79,6 +79,22 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             this.label1.TabIndex = 32;
             this.label1.Text = "Amount";
             // 
+            // nudAmount
+            // 
+            this.nudAmount.DecimalPlaces = 2;
+            this.nudAmount.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nudAmount.Location = new System.Drawing.Point(80, 62);
+            this.nudAmount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nudAmount.Maximum = new decimal(new int[] {
+            1316134911,
+            2328,
+            0,
+            0});
+            this.nudAmount.Name = "nudAmount";
+            this.nudAmount.Size = new System.Drawing.Size(260, 22);
+            this.nudAmount.TabIndex = 30;
+            this.nudAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -90,38 +106,22 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             this.label6.Text = "Description ";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
-            // numericUpDown1
+            // txtDescription
             // 
-            this.numericUpDown1.DecimalPlaces = 2;
-            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.numericUpDown1.Location = new System.Drawing.Point(80, 62);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            1316134911,
-            2328,
-            0,
-            0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(260, 22);
-            this.numericUpDown1.TabIndex = 30;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            // 
-            // txtnature
-            // 
-            this.txtnature.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtnature.Location = new System.Drawing.Point(87, 23);
-            this.txtnature.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtnature.MaxLength = 150;
-            this.txtnature.Multiline = true;
-            this.txtnature.Name = "txtnature";
-            this.txtnature.Size = new System.Drawing.Size(260, 37);
-            this.txtnature.TabIndex = 31;
+            this.txtDescription.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDescription.Location = new System.Drawing.Point(87, 23);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDescription.MaxLength = 150;
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(260, 37);
+            this.txtDescription.TabIndex = 31;
             // 
             // groupBox2
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.dgDeductions);
             this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.groupBox2.Location = new System.Drawing.Point(7, 128);
             this.groupBox2.Name = "groupBox2";
@@ -130,14 +130,14 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "List";
             // 
-            // dataGridView1
+            // dgDeductions
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(10, 21);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(351, 121);
-            this.dataGridView1.TabIndex = 0;
+            this.dgDeductions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDeductions.Location = new System.Drawing.Point(10, 21);
+            this.dgDeductions.Name = "dgDeductions";
+            this.dgDeductions.RowTemplate.Height = 25;
+            this.dgDeductions.Size = new System.Drawing.Size(351, 121);
+            this.dgDeductions.TabIndex = 0;
             // 
             // frmDeductions
             // 
@@ -145,7 +145,7 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(386, 282);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.txtnature);
+            this.Controls.Add(this.txtDescription);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -158,9 +158,9 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
             this.Text = "Deductions";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
             this.groupBox2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDeductions)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,10 +171,10 @@ namespace AccountingSystem.Views.Transactions.CheckIssuance.Deductions
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label label6;
-        internal System.Windows.Forms.NumericUpDown numericUpDown1;
-        internal System.Windows.Forms.TextBox txtnature;
+        internal System.Windows.Forms.NumericUpDown nudAmount;
+        internal System.Windows.Forms.TextBox txtDescription;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgDeductions;
     }
 }
