88f2ca9b9a9054963e3ab5fc4633ae214a6dd574
archie201998 <px1998.ass@gmail.com>
2022-02-28 13:36:34 +0800

    Added prefix and suffix on disbursing officer list

--- ACC.Data/DisbursingOfficerRepository.cs
+++ ACC.Data/DisbursingOfficerRepository.cs
@@ -130,7 +130,7 @@ namespace ACC.Data
         {
             try
             {
-                string query = $"SELECT id, CONCAT(`first_name`, ' ', `mid_initial`, ' ', `last_name`) as fullname, job_title,  created_at, updated_at FROM {tableName}";
+                string query = $"SELECT id, CONCAT(`first_name`, ' ', `mid_initial`, ' ', `last_name`) as fullname, job_title,  created_at, updated_at, users_id FROM {tableName}";
 
                 return _dbGenericCommands.Fill(query, new DataTable());
             }
--- AccountingSystem/HelperLoadRecords.cs
+++ AccountingSystem/HelperLoadRecords.cs
@@ -1285,6 +1285,7 @@ namespace AccountingSystem
             datagrid.Columns[2].HeaderText = "Job Title";
             datagrid.Columns[3].Visible = false;
             datagrid.Columns[4].Visible = false;
+            datagrid.Columns[5].Visible = false;
 
             datagrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
