d722952902ec90a389019ffa31e0ff5a3223e702
archie201998 <px1998.ass@gmail.com>
2022-03-07 10:30:48 +0800

    Added method in CollectingOfficerHasJobOrders to get the regular collector Id, when receipt will be issued to JO collector.

--- ACC.Domain/Interfaces/ICollectingOfficerHasJobOrders.cs
+++ ACC.Domain/Interfaces/ICollectingOfficerHasJobOrders.cs
@@ -9,7 +9,7 @@ namespace ACC.Domain.Interfaces
     public interface ICollectingOfficerHasJobOrders : IRepository<CollectingOfficerHasJobOrdersModel>
     {
         DataTable GetRecordsBySearch(int collectingOfficerId, string searchText);
-
         DataTable GetJobOrdersByCollectingOfficerId(int collectingOfficerId);
+        int GetCollectingOfficerIDByJobOrderId(int collectingOfficerId);
     }
 }
