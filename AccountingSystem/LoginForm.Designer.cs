ac87582001695757d50582ceda006e598843cf8b
archie201998 <px1998.ass@gmail.com>
2022-03-07 11:47:05 +0800

    Removed username and password default text.

--- AccountingSystem/LoginForm.Designer.cs
+++ AccountingSystem/LoginForm.Designer.cs
@@ -54,7 +54,6 @@ namespace AccountingSystem
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(322, 23);
             this.txtUsername.TabIndex = 0;
-            this.txtUsername.Text = "rudy";
             // 
             // label1
             // 
@@ -84,7 +83,6 @@ namespace AccountingSystem
             this.txtPassword.PasswordChar = '•';
             this.txtPassword.Size = new System.Drawing.Size(322, 23);
             this.txtPassword.TabIndex = 2;
-            this.txtPassword.Text = "123";
             // 
             // panel1
             // 
