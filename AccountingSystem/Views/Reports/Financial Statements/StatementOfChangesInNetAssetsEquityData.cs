0cfda5046c365a2284db36e1cabb62f3f17ac7dc
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-05-23 16:23:29 +0800

    Used dictionary on gettting data of SCNAE report.

---
+++ AccountingSystem/Views/Reports/Financial Statements/StatementOfChangesInNetAssetsEquityData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccountingSystem.Views.Reports.Financial_Statements
+{
+    public partial class StatementOfChangesInNetAssetsEquityData
+    {
+
+        internal decimal GetSurplusDeficitForThePeriod(byte fundId, DateTime date)
+        {
+            decimal statementOfFinancialPerformanceData = new StatementOfFinancialPerformanceData(fundId, date).SurplusDeficitPeriod();
+
+            return statementOfFinancialPerformanceData;
+        }
+
+        private decimal GetStartingBalance(byte fundId, DateTime date)
+        {
+            var dictBeginningBalance = Factory.BeginningBalancesRepository().GetSumBalancesBy_FundId_GenLedgId_Date_SubLedgId(fundId, 331, date);
+            decimal beginningBalanceDebit = dictBeginningBalance["beginning_balance_debit"];
+            decimal beginningBalanceCredit = dictBeginningBalance["beginning_balance_credit"];
+            decimal beginningBalance = beginningBalanceDebit - beginningBalanceCredit;
+
+            return Math.Abs(beginningBalance);
+        }
+
+        internal Dictionary<string, dynamic> GetStatementOfChangesOfAssetsEquity(byte fundId, DateTime present, DateTime previous)
+        {
+            var dict = new Dictionary<string, dynamic>();
+
+            dict.Add("present_starting_balance", GetStartingBalance(fundId, present));
+            dict.Add("previous_starting_balance", GetStartingBalance(fundId, previous));
+            dict.Add("present_surplus_deficits_for_the_period", GetSurplusDeficitForThePeriod(fundId, present));
+            dict.Add("previous_surplus_deficits_for_the_period", GetSurplusDeficitForThePeriod(fundId, previous));
+
+            return dict;
+        }
+    }
+}
