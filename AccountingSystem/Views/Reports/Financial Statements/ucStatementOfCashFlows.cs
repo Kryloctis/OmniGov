63d27308d19fda041bcc3f91e6b16d27675f0ae8
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-05-24 16:58:30 +0800

    Created a class and functions to get data for SCF report.

--- AccountingSystem/Views/Reports/Financial Statements/ucStatementOfCashFlows.cs
+++ AccountingSystem/Views/Reports/Financial Statements/ucStatementOfCashFlows.cs
@@ -20,11 +20,41 @@ namespace AccountingSystem.Views.Reports.Financial_Statements
 
         private DataTable SCFDatatable()
         {
+            byte fundId = Convert.ToByte(cmbxFunds.SelectedValue);
+            DateTime date = dtPickerDate.Value;
+
             var dataTable = new dsLFS.dtStatementOfCashFlowsDataTable();
+            var dict = new StatementOfCashFlowsData().GetStatementOfCashFlowsData(fundId, date);
 
             var records = new object[]
             {
-                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
+                dict["collection_from_taxpayers"],
+                dict["share_from_internal_revenue_allotment"],
+                dict["receipts_from_business_services_income"],
+                dict["interest_income"],
+                dict["dividend_income"],
+                dict["other_receipts"],
+                dict["payment_of_expenses"],
+                dict["payments_to_suppliers_and_creditors"],
+                dict["payments_to_employees"],
+                dict["interest_expense"],
+                dict["other_expenses"],
+                dict["proceeds_from_sale_if_investment_property"],
+                dict["proceeds_from_sale_disposal_of_property_plant_and_equipment"],
+                dict["proceeds_from_sale_of_non_current_investments"],
+                dict["collection_of_principal_on_loans_to_other_entities"],
+                dict["purchase_construction_of_investment_property"],
+                dict["purchase_construction_of_property_plant_and_equipment"],
+                dict["investment"],
+                dict["purchase_of_bearer_biological_assets"],
+                dict["purchase_of_intangible_assets"],
+                dict["grant_of_loans"],
+                dict["proceeds_from_issuance_of_bonds"],
+                dict["proceeds_from_loans"],
+                dict["payment_of_long_term_liabilities"],
+                dict["retirement_redemption_of_debt_securities"],
+                dict["payment_of_loan_amortization"],
+                dict["cash_at_the_end_of_month"]
             };
 
 
