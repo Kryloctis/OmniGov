fbce9bac50b7d8644ca32b15d86d5e28f7c3adca
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-05-25 11:04:54 +0800

    Fixed error in Subsidiary Ledger report date.

--- AccountingSystem/Views/Reports/Ledgers/ucSubsidiaryLedger.cs
+++ AccountingSystem/Views/Reports/Ledgers/ucSubsidiaryLedger.cs
@@ -138,11 +138,14 @@ namespace AccountingSystem.Views.Reports.Ledgers
 
             beginningBalance = (DebitBeginningBalance - CreditBeginningBalance);
             balance = beginningBalance.ToString();
-            balanceDebit = DebitBeginningBalance > CreditBeginningBalance ? Math.Abs(beginningBalance).ToString() : string.Empty;
-            balanceCredit = DebitBeginningBalance < CreditBeginningBalance ? Math.Abs(beginningBalance).ToString() : string.Empty;
+            decimal debit = DebitBeginningBalance > CreditBeginningBalance ? Math.Abs(beginningBalance) : 0;
+            decimal credit = DebitBeginningBalance < CreditBeginningBalance ? Math.Abs(beginningBalance) : 0;
+
+            balanceDebit = debit.ToString();
+            balanceCredit = credit.ToString();
 
             var dateDict = Factory.BeginningBalancesRepository().GetRecordBy_FundId_GenLedgId_Year_SubLedgId(fundId, generalLedgerId, year, subsidiaryLedgerId);
-            balanceDate = string.IsNullOrEmpty(dateDict["date_entry"]) ? string.Empty : Convert.ToDateTime(dateDict["date_entry"]).ToString("MMM,dd,yyyy");
+            balanceDate = string.IsNullOrEmpty(dateDict["date_entry"]) ? string.Empty : Convert.ToDateTime(dateDict["date_entry"]).ToString("MMM dd, yy");
         }
 
         private void LoadReport(LocalReport report)
