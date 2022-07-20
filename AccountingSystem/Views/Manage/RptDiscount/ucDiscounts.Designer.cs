eefc82f11a1a0c79bcd5a03cda963710151ad8d4
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-07-20 14:46:46 +0800

    Added user control for discounts.

---
+++ AccountingSystem/Views/Manage/RptDiscount/ucDiscounts.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace AccountingSystem.Views.Manage.RptDiscount
+{
+    partial class ucDiscounts
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.nudRate = new System.Windows.Forms.NumericUpDown();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Rate";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(93, 3);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(321, 27);
+            this.txtCode.TabIndex = 1;
+            this.txtCode.Validating += new System.ComponentModel.CancelEventHandler(this.txtCode_Validating);
+            this.txtCode.Validated += new System.EventHandler(this.txtCode_Validated);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(93, 36);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(321, 76);
+            this.txtDescription.TabIndex = 1;
+            this.txtDescription.Validating += new System.ComponentModel.CancelEventHandler(this.txtDescription_Validating);
+            this.txtDescription.Validated += new System.EventHandler(this.txtDescription_Validated);
+            // 
+            // nudRate
+            // 
+            this.nudRate.DecimalPlaces = 2;
+            this.nudRate.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudRate.Location = new System.Drawing.Point(93, 118);
+            this.nudRate.Name = "nudRate";
+            this.nudRate.Size = new System.Drawing.Size(321, 27);
+            this.nudRate.TabIndex = 2;
+            this.nudRate.Validating += new System.ComponentModel.CancelEventHandler(this.nudRate_Validating);
+            this.nudRate.Validated += new System.EventHandler(this.nudRate_Validated);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ucDiscounts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.nudRate);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ucDiscounts";
+            this.Size = new System.Drawing.Size(436, 151);
+            ((System.ComponentModel.ISupportInitialize)(this.nudRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.NumericUpDown nudRate;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ AccountingSystem/Views/Manage/RptDiscount/ucDiscounts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Manage.RptDiscount
+{
+    public partial class ucDiscounts : UserControl
+    {
+        public ucDiscounts()
+        {
+            InitializeComponent();
+        }
+
+        #region Validations
+        private void txtCode_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCode_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void txtDescription_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDescription_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void nudRate_Validated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudRate_Validating(object sender, CancelEventArgs e)
+        {
+
+        } 
+        #endregion
+    }
+}
