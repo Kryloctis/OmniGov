4721c91d2dbfb0ee4196beacc6dd34e20bf4c912
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-07-21 13:37:28 +0800

    Added main form for tax rates.

---
+++ AccountingSystem/Views/Manage/RptTaxRates/frmRptTaxRates.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Manage.RptTaxRates
+{
+    public partial class frmRptTaxRates : Form
+    {
+        private readonly MainForm _mainForm;
+        public frmRptTaxRates(MainForm mainForm)
+        {
+            InitializeComponent();
+            _mainForm = mainForm;
+            Helper.LoadFormIcon(this);
+        }
+
+        private void LoadTaxRates() 
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            Helper.EnableDisableToolStripButtons(dataGridView1, btnEdit, btnDelete);
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void frmRptTaxRates_Load(object sender, EventArgs e)
+        {
+            Helper.EnableDisableToolStripButtons(dataGridView1, btnEdit, btnDelete);
+        }
+    }
+}
---
+++ AccountingSystem/Views/Manage/RptTaxRates/ucRptTaxRates.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace AccountingSystem.Views.Manage.RptTaxRates
+{
+    partial class ucRptTaxRates
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nudRate = new System.Windows.Forms.NumericUpDown();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudRate
+            // 
+            this.nudRate.DecimalPlaces = 2;
+            this.nudRate.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudRate.Location = new System.Drawing.Point(82, 88);
+            this.nudRate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nudRate.Name = "nudRate";
+            this.nudRate.Size = new System.Drawing.Size(281, 23);
+            this.nudRate.TabIndex = 8;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(82, 27);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(281, 58);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(82, 2);
+            this.txtCode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(281, 23);
+            this.txtCode.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rate (%)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Code";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ucRptTaxRates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.nudRate);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ucRptTaxRates";
+            this.Size = new System.Drawing.Size(382, 113);
+            ((System.ComponentModel.ISupportInitialize)(this.nudRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.NumericUpDown nudRate;
+        internal System.Windows.Forms.TextBox txtDescription;
+        internal System.Windows.Forms.TextBox txtCode;
+        internal System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
