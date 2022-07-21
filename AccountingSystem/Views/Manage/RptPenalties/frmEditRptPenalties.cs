47da57521a3c20d4a4b14acfc51470523328e7b0
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-07-21 10:27:33 +0800

    Added edit form for rpt penalties.

---
+++ AccountingSystem/Views/Manage/RptPenalties/frmEditRptPenalties.cs
@@ -0,0 +1,64 @@
+﻿using ACC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Manage.RptPenalties
+{
+    public partial class frmEditRptPenalties : Form
+    {
+        private readonly frmRptPenalties _frmRptPenalties;
+        private ucRptPenalties uc;
+        public frmEditRptPenalties(int rptPenaltiesId, frmRptPenalties frmRptPenalties)
+        {
+            InitializeComponent();
+            _frmRptPenalties = frmRptPenalties;
+            uc = ucRptPenalties1;
+            uc.rptPenaltiesId = rptPenaltiesId;
+            Helper.LoadFormIcon(this);
+        }
+
+        private bool Save() 
+        {
+            try
+            {
+                if (!uc.ValidateChildren())
+                {
+                    Helper.MessageBoxError(uc.GetFormErrors());
+                    return false;
+                }
+
+                var model = new RptPenaltiesModel()
+                {
+                    Id = uc.rptPenaltiesId,
+                    Code = uc.txtCode.Text.Trim(),
+                    Description = uc.txtDescription.Text.Trim(),
+                    Rate = uc.nudRate.Value
+                };
+
+                return Factory.rptPenaltiesRepository().Update(model);
+            }
+            catch (Exception ex)
+            {
+                Helper.MessageBoxError(ex.Message);
+            }
+            return false;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (Save())
+            {
+                Helper.MessageBoxSuccess("Penalty has been updated.");
+                _frmRptPenalties.LoadPenalties();
+                Close();
+            }
+        }
+    }
+}
