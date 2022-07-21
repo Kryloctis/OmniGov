79cc6aaef6b0d4c2e1d10428cbdfde7209cba9c9
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-07-21 09:56:20 +0800

    Added repository for rpt penalties.

---
+++ ACC.Data/RptPenaltiesRepository.cs
@@ -0,0 +1,59 @@
+﻿using ACC.Domain.Interfaces;
+using ACC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ACC.Data
+{
+    public class RptPenaltiesRepository : IRptPenaltiesRepository
+    {
+        private MySqlGenericCommands _mySqlGenericCommandsLFS;
+
+        public RptPenaltiesRepository(MySqlGenericCommands mySqlGenericCommandsLFS)
+        {
+            _mySqlGenericCommandsLFS = mySqlGenericCommandsLFS;
+        }
+
+        public int CountRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(List<RptPenaltiesModel> entityList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(RptPenaltiesModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(RptPenaltiesModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
