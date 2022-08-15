fc0a82c5e72342e948c50ee262f0a38052afc5c4
archie201998 <px1998.ass@gmail.com>
2022-08-15 10:57:07 +0800

    Fixed query to retrive collectors report payment collection

--- ACC.Data/CollectorReportPaymentsRepository.cs
+++ ACC.Data/CollectorReportPaymentsRepository.cs
@@ -213,7 +213,7 @@ namespace ACC.Data
                 new object[]{ "@report_no", DbType.String, reportNo},
             };
 
-            string query = $"SELECT payment_collections_id, funds_id, fund_name, accountable_form_id, account_code, accountable_forms, general_ledger_accounts_id, ledger_name, payee, LPAD(receipt_no, 7, 0) AS receipt_no, quantity, payment_date, amount FROM {viewTableName} WHERE report_no = @report_no";
+            string query = $"SELECT payment_collections_id, funds_id, fund_name, accountable_forms_id, account_code, accountable_forms_no, accountable_forms_desc, accountable_forms, general_ledger_accounts_id, ledger_name, payee, LPAD(receipt_no, 7, 0) AS receipt_no, quantity, payment_date, amount FROM {viewTableName} WHERE report_no = @report_no";
 
             var dtpc = new DataTable();
             return _dbGenericCommands.FillBySearch(query, dtpc, parameter);
