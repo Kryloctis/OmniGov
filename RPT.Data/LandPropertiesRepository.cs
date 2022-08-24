140915364affdae5754f20135d24b3ee5a41ddf1
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-08-24 11:25:47 +0800

    Added land properties model, interface and repository.

---
+++ RPT.Data/LandPropertiesRepository.cs
@@ -0,0 +1,62 @@
+﻿using ACC.Data;
+using RPT.Domain.Interfaces;
+using RPT.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPT.Data
+{
+    public class LandPropertiesRepository : ILandPropertiesRepository
+    {
+        private MySqlGenericCommands mySqlGenericCommandsRPT;
+
+        public LandPropertiesRepository(MySqlGenericCommands mySqlGenericCommandsRPT)
+        {
+            this.mySqlGenericCommandsRPT = mySqlGenericCommandsRPT;
+        }
+
+        public int CountRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(List<LandPropertiesModel> entityList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(LandPropertiesModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LandPropertiesModel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RPT.Domain/Interfaces/ILandPropertiesRepository.cs
@@ -0,0 +1,14 @@
+﻿using ACC.Domain.Interfaces;
+using RPT.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPT.Domain.Interfaces
+{
+    public interface ILandPropertiesRepository : IRepository<LandPropertiesModel>
+    {
+    }
+}
