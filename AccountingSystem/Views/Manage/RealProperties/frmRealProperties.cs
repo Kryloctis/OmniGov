b22a0bfe429afe82c1bb525ed4d148892c8e7436
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-09-01 15:09:21 +0800

    Added Real Properties form.

---
+++ AccountingSystem/Views/Manage/RealProperties/frmRealProperties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Manage.RealProperties
+{
+    public partial class frmRealProperties : Form
+    {
+        public frmRealProperties()
+        {
+            Helper.LoadFormIcon(this);
+            InitializeComponent();
+            Helper.DatagridFullRowSelectStyle(dataGridView1, true);
+        }
+
+        private void frmRealProperties_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
