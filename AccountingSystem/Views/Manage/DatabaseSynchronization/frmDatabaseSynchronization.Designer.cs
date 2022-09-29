1e3aaf83d14cd74721462b0cf2edd85d46c00b86
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-09-29 13:28:43 +0800

    Created form for database synchronization.

---
+++ AccountingSystem/Views/Manage/DatabaseSynchronization/frmDatabaseSynchronization.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AccountingSystem.Views.Manage.DatabaseSynchronization
+{
+    partial class frmDatabaseSynchronization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.cmbxSyncType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 73);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(363, 5);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // cmbxSyncType
+            // 
+            this.cmbxSyncType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbxSyncType.FormattingEnabled = true;
+            this.cmbxSyncType.Location = new System.Drawing.Point(19, 36);
+            this.cmbxSyncType.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.cmbxSyncType.Name = "cmbxSyncType";
+            this.cmbxSyncType.Size = new System.Drawing.Size(325, 23);
+            this.cmbxSyncType.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose what to sync:";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.flowLayoutPanel1.Controls.Add(this.btnStop);
+            this.flowLayoutPanel1.Controls.Add(this.btnSync);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 78);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(363, 32);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(252, 3);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(51, 23);
+            this.btnSync.TabIndex = 0;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(309, 3);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(51, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // frmDatabaseSynchronization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 110);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbxSyncType);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmDatabaseSynchronization";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Database Synchronization";
+            this.Load += new System.EventHandler(this.frmDatabaseSynchronization_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox cmbxSyncType;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
---
+++ AccountingSystem/Views/Manage/DatabaseSynchronization/frmDatabaseSynchronization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Manage.DatabaseSynchronization
+{
+    public partial class frmDatabaseSynchronization : Form
+    {
+        public frmDatabaseSynchronization()
+        {
+            InitializeComponent();
+            Helper.LoadFormIcon(this);
+        }
+
+        private void LoadSyncOptions()
+        {
+            var dict = new Dictionary<string, string>()
+            {
+                { "RPT", "0"}
+            };
+
+            var bindingSource = new BindingSource(dict, null);
+            cmbxSyncType.DataSource = bindingSource;
+            cmbxSyncType.DisplayMember = "Key";
+            cmbxSyncType.ValueMember = "Value";
+        }
+
+        private void frmDatabaseSynchronization_Load(object sender, EventArgs e)
+        {
+            LoadSyncOptions();
+        }
+    }
+}
