0e6f423d162c271828c50b3fe7c36b4c3ead56f5
Janrey Ubane <janrey_ubane25@yahoo.com>
2022-09-29 11:40:43 +0800

    Created model, interface, repositories and API for taxpayers.

---
+++ ACC.Domain/Interfaces/ITaxpayers.cs
@@ -0,0 +1,10 @@
+﻿using ACC.Domain.Models;
+
+namespace ACC.Domain.Interfaces
+{
+    public interface ITaxpayers : IRepository<TaxpayersModel>
+    {
+        bool ITaxpayerNameExist(string name);
+        bool ITaxpayerNameExist(int id, string name);
+    }
+}
