8c8c0311215bd9b5e5d398c3778cfbd6bee1fdaf
A R C H I E <px1998.ass@gmail.com>
2022-11-11 09:58:42 +0800

    Created usercontrol, add, edit, and main form for business categories.

---
+++ AccountingSystem/Views/Manage/BusinessCategories/ucBusinessCategories.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace AccountingSystem.Views.Manage.BusinessCategories
+{
+    partial class ucBusinessCategories
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ucBusinessCategories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ucBusinessCategories";
+            this.Size = new System.Drawing.Size(477, 371);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
