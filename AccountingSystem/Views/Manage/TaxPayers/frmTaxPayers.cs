4652a390c5f13a95ceb604dde3fe5ee577789e7b
A R C H I E <px1998.ass@gmail.com>
2022-12-15 10:43:59 +0800

    Fix taxpayer list record display, Concatenate Street, Barangay, Municipality, and Province into 1 column name Address.

--- AccountingSystem/Views/Manage/TaxPayers/frmTaxPayers.cs
+++ AccountingSystem/Views/Manage/TaxPayers/frmTaxPayers.cs
@@ -66,7 +66,7 @@ namespace AccountingSystem.Views.Manage.TaxPayers
 
                 dataTable.Rows.Add(newRow);
             }
-            return dataTable;
+            return dataTable;   
         }
 
         internal void LoadTaxpayers()
