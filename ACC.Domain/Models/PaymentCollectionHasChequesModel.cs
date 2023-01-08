d1a7b6b8e9ddcc42857e59161d63896d657a4181
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-01-08 12:51:29 +0800

    Added model for Payment Collection has Cheques.

---
+++ ACC.Domain/Models/PaymentCollectionHasChequesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACC.Domain.Models
+{
+    public class PaymentCollectionHasChequesModel
+    {
+        public int PaymentCollectionId { get; set; }
+        public int ChequesId { get; set; }
+    }
+}
