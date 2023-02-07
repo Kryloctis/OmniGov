c03fa189e594d564dcdae6e9d1aed6b72a5eb715
A R C H I E <px1998.ass@gmail.com>
2023-02-07 09:16:17 +0800

    Fixed status bar information of created at and updated by.

--- ACC.Data/RCIRepository.cs
+++ ACC.Data/RCIRepository.cs
@@ -181,7 +181,7 @@ namespace ACC.Data
                     new object[] { "@id", DbType.Int16, id },
                 };
 
-                string query = $"SELECT id FROM {tableName} WHERE id = @id";
+                string query = $"SELECT id FROM {tableName} WHERE id = @id";    
                 string queryResult = _dbGenericCommands.ExecuteScalar(query, parameters);
 
                 // if query is not null, means found some record, so true
