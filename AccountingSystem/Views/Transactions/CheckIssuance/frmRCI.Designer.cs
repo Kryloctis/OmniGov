4548e8387c702f09c627f3b2d70154c45a9a3502
A R C H I E <px1998.ass@gmail.com>
2023-02-21 01:56:50 +0800

    Changed check issuance form window state into normal.

--- AccountingSystem/Views/Transactions/CheckIssuance/frmRCI.Designer.cs
+++ AccountingSystem/Views/Transactions/CheckIssuance/frmRCI.Designer.cs
@@ -210,7 +210,6 @@ namespace AccountingSystem.Views.Transactions.RCI
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Issue Check";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmRCI_Load);
             this.toolStrip.ResumeLayout(false);
             this.toolStrip.PerformLayout();
