d4401341c38a2641174a396c39e92b522ba5d7c4
A R C H I E <px1998.ass@gmail.com>
2023-04-18 13:58:46 +0800

    Created UI for accountable for 51 and 57

---
+++ AccountingSystem/Views/Transactions/Payments/OtherPayments/AF51&57/ucAF51&57.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace AccountingSystem.Views.Transactions.Payments.OtherPayments.AF51_57
+{
+    partial class ucAF51_57
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            dataGridView1 = new System.Windows.Forms.DataGridView();
+            button3 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            panel1 = new System.Windows.Forms.Panel();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            panel2 = new System.Windows.Forms.Panel();
+            textBox3 = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox2.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(panel1);
+            groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            groupBox1.Location = new System.Drawing.Point(4, 69);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new System.Windows.Forms.Padding(7);
+            groupBox1.Size = new System.Drawing.Size(701, 350);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Charges";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new System.Drawing.Point(329, 23);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new System.Drawing.Size(362, 320);
+            dataGridView1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button3.Location = new System.Drawing.Point(292, 225);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(31, 23);
+            button3.TabIndex = 1;
+            button3.Text = "x";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(292, 196);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(31, 23);
+            button2.TabIndex = 1;
+            button2.Text = "<";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(292, 167);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(31, 23);
+            button1.TabIndex = 1;
+            button1.Text = ">";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.Color.White;
+            panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            panel1.Location = new System.Drawing.Point(7, 23);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(279, 320);
+            panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(panel2);
+            groupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            groupBox2.Location = new System.Drawing.Point(4, 4);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new System.Drawing.Size(701, 59);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Taxpayer Information";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label1);
+            panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            panel2.Location = new System.Drawing.Point(3, 19);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(695, 37);
+            panel2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(493, 8);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(195, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(438, 11);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(49, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Contact";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(331, 8);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(78, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(294, 11);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(31, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Type";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(60, 8);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(223, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(15, 11);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name";
+            // 
+            // ucAF51_57
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox2);
+            Name = "ucAF51_57";
+            Padding = new System.Windows.Forms.Padding(4);
+            Size = new System.Drawing.Size(711, 424);
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox2.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ AccountingSystem/Views/Transactions/Payments/OtherPayments/AF51&57/ucAF51&57.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Transactions.Payments.OtherPayments.AF51_57
+{
+    public partial class ucAF51_57 : UserControl
+    {
+        public ucAF51_57()
+        {
+            InitializeComponent();
+        }
+    }
+}
