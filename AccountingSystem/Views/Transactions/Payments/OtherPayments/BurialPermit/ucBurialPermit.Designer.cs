674fc0343a989d130698504c0907edfffa859846
A R C H I E <px1998.ass@gmail.com>
2023-04-19 09:55:55 +0800

    Updated Marriage license user control.

---
+++ AccountingSystem/Views/Transactions/Payments/OtherPayments/BurialPermit/ucBurialPermit.Designer.cs
@@ -0,0 +1,407 @@
+﻿namespace AccountingSystem.Views.Transactions.Payments.OtherPayments.BurialPermit
+{
+    partial class ucBurialPermit
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            panel2 = new System.Windows.Forms.Panel();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            panel1 = new System.Windows.Forms.Panel();
+            textBox3 = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            textBox5 = new System.Windows.Forms.TextBox();
+            label6 = new System.Windows.Forms.Label();
+            comboBox1 = new System.Windows.Forms.ComboBox();
+            groupBox3 = new System.Windows.Forms.GroupBox();
+            panel3 = new System.Windows.Forms.Panel();
+            dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            label7 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            textBox6 = new System.Windows.Forms.TextBox();
+            textBox7 = new System.Windows.Forms.TextBox();
+            label9 = new System.Windows.Forms.Label();
+            textBox8 = new System.Windows.Forms.TextBox();
+            label10 = new System.Windows.Forms.Label();
+            checkBox1 = new System.Windows.Forms.CheckBox();
+            label11 = new System.Windows.Forms.Label();
+            label12 = new System.Windows.Forms.Label();
+            checkBox2 = new System.Windows.Forms.CheckBox();
+            groupBox2.SuspendLayout();
+            panel2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            panel1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(panel2);
+            groupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            groupBox2.Location = new System.Drawing.Point(3, 3);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new System.Windows.Forms.Padding(5);
+            groupBox2.Size = new System.Drawing.Size(299, 206);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Taxpayer's Info.";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label1);
+            panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            panel2.Location = new System.Drawing.Point(5, 21);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(289, 180);
+            panel2.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(77, 47);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(203, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(6, 47);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(65, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Permission";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(77, 18);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(203, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(6, 18);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(panel1);
+            groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            groupBox1.Location = new System.Drawing.Point(308, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            groupBox1.Size = new System.Drawing.Size(303, 206);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Taxpayer's Info.";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(label4);
+            panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            panel1.Location = new System.Drawing.Point(5, 21);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(293, 180);
+            panel1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(77, 47);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(203, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(6, 47);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(65, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Permission";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new System.Drawing.Point(77, 18);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(203, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(6, 18);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(39, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(6, 76);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(28, 15);
+            label5.TabIndex = 2;
+            label5.Text = "Age";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new System.Drawing.Point(77, 76);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(99, 23);
+            textBox5.TabIndex = 3;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(6, 105);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(25, 15);
+            label6.TabIndex = 2;
+            label6.Text = "Sex";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new System.Drawing.Point(77, 105);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(99, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(panel3);
+            groupBox3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            groupBox3.Location = new System.Drawing.Point(617, 5);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Padding = new System.Windows.Forms.Padding(5);
+            groupBox3.Size = new System.Drawing.Size(362, 207);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Death Details";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(checkBox2);
+            panel3.Controls.Add(checkBox1);
+            panel3.Controls.Add(dateTimePicker1);
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label12);
+            panel3.Controls.Add(label11);
+            panel3.Controls.Add(label10);
+            panel3.Controls.Add(label9);
+            panel3.Controls.Add(label8);
+            panel3.Controls.Add(textBox8);
+            panel3.Controls.Add(textBox7);
+            panel3.Controls.Add(textBox6);
+            panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            panel3.Location = new System.Drawing.Point(5, 21);
+            panel3.Name = "panel3";
+            panel3.Size = new System.Drawing.Size(352, 181);
+            panel3.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new System.Drawing.Point(99, 18);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(5, 21);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(79, 15);
+            label7.TabIndex = 2;
+            label7.Text = "Date of Death";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new System.Drawing.Point(6, 47);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(59, 15);
+            label8.TabIndex = 2;
+            label8.Text = "Cementry";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new System.Drawing.Point(99, 45);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new System.Drawing.Size(200, 23);
+            textBox6.TabIndex = 3;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new System.Drawing.Point(99, 74);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new System.Drawing.Size(200, 23);
+            textBox7.TabIndex = 3;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new System.Drawing.Point(6, 76);
+            label9.Name = "label9";
+            label9.Size = new System.Drawing.Size(79, 15);
+            label9.TabIndex = 2;
+            label9.Text = "Disintertment";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new System.Drawing.Point(99, 103);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new System.Drawing.Size(200, 23);
+            textBox8.TabIndex = 3;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new System.Drawing.Point(6, 105);
+            label10.Name = "label10";
+            label10.Size = new System.Drawing.Size(66, 15);
+            label10.TabIndex = 2;
+            label10.Text = "Disposition";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(99, 135);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(15, 14);
+            checkBox1.TabIndex = 4;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new System.Drawing.Point(6, 132);
+            label11.Name = "label11";
+            label11.Size = new System.Drawing.Size(59, 15);
+            label11.TabIndex = 2;
+            label11.Text = "Infectious";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new System.Drawing.Point(6, 156);
+            label12.Name = "label12";
+            label12.Size = new System.Drawing.Size(64, 15);
+            label12.TabIndex = 2;
+            label12.Text = "Embalmed";
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new System.Drawing.Point(99, 159);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new System.Drawing.Size(15, 14);
+            checkBox2.TabIndex = 4;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // ucBurialPermit
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox2);
+            Name = "ucBurialPermit";
+            Size = new System.Drawing.Size(1003, 212);
+            groupBox2.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+    }
+}
