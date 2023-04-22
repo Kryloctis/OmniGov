a1295321c1c4b6b69edb91af7255e474563d4f65
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-04-22 19:21:16 +0800

    Fixed Allotment Classes UI, codings.

--- AccountingSystem/Views/Manage/AllotmentClasses/frmAllotmentClasses.Designer.cs
+++ AccountingSystem/Views/Manage/AllotmentClasses/frmAllotmentClasses.Designer.cs
@@ -30,177 +30,184 @@ namespace AccountingSystem.Views.Manage.AllotmentClasses
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAllotmentClasses));
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.btnAdd = new System.Windows.Forms.ToolStripButton();
-            this.btnEdit = new System.Windows.Forms.ToolStripButton();
-            this.btnDelete = new System.Windows.Forms.ToolStripButton();
-            this.dgAllotmentClasses = new System.Windows.Forms.DataGridView();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.lblRecordCount = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.lblCreatedAt = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.lblUpdatedAt = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgAllotmentClasses)).BeginInit();
-            this.statusStrip1.SuspendLayout();
-            this.SuspendLayout();
+            toolStrip1 = new System.Windows.Forms.ToolStrip();
+            btnAdd = new System.Windows.Forms.ToolStripButton();
+            btnEdit = new System.Windows.Forms.ToolStripButton();
+            btnDelete = new System.Windows.Forms.ToolStripButton();
+            txtSearch = new System.Windows.Forms.ToolStripTextBox();
+            statusStrip1 = new System.Windows.Forms.StatusStrip();
+            toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            lblRecordCount = new System.Windows.Forms.ToolStripStatusLabel();
+            toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            lblCreatedAt = new System.Windows.Forms.ToolStripStatusLabel();
+            toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            lblUpdatedAt = new System.Windows.Forms.ToolStripStatusLabel();
+            panel1 = new System.Windows.Forms.Panel();
+            dgAllotmentClasses = new System.Windows.Forms.DataGridView();
+            toolStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgAllotmentClasses).BeginInit();
+            SuspendLayout();
             // 
             // toolStrip1
             // 
-            this.toolStrip1.BackColor = System.Drawing.SystemColors.Control;
-            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnAdd,
-            this.btnEdit,
-            this.btnDelete});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Padding = new System.Windows.Forms.Padding(10, 2, 1, 0);
-            this.toolStrip1.Size = new System.Drawing.Size(699, 52);
-            this.toolStrip1.TabIndex = 4;
-            this.toolStrip1.Text = "toolStrip1";
-            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            toolStrip1.BackColor = System.Drawing.SystemColors.Control;
+            toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { btnAdd, btnEdit, btnDelete, txtSearch });
+            toolStrip1.Location = new System.Drawing.Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Padding = new System.Windows.Forms.Padding(4);
+            toolStrip1.Size = new System.Drawing.Size(729, 50);
+            toolStrip1.TabIndex = 4;
+            toolStrip1.Text = "toolStrip1";
+            toolStrip1.ItemClicked += toolStrip1_ItemClicked;
             // 
             // btnAdd
             // 
-            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
-            this.btnAdd.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(33, 47);
-            this.btnAdd.Text = "Add";
-            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            btnAdd.Image = Properties.Resources.button_rounded_add_20px;
+            btnAdd.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new System.Drawing.Size(42, 39);
+            btnAdd.Text = "Add...";
+            btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            btnAdd.Click += btnAdd_Click;
             // 
             // btnEdit
             // 
-            this.btnEdit.Image = ((System.Drawing.Image)(resources.GetObject("btnEdit.Image")));
-            this.btnEdit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(32, 47);
-            this.btnEdit.Text = "Edit";
-            this.btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            btnEdit.Image = Properties.Resources.button_rounded_edit_20px;
+            btnEdit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            btnEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new System.Drawing.Size(40, 39);
+            btnEdit.Text = "Edit...";
+            btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            btnEdit.Click += btnEdit_Click;
             // 
             // btnDelete
             // 
-            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
-            this.btnDelete.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(44, 47);
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            btnDelete.Image = Properties.Resources.button_rounded_remove_20px;
+            btnDelete.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            btnDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new System.Drawing.Size(44, 39);
+            btnDelete.Text = "Delete";
+            btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            btnDelete.Click += btnDelete_Click;
             // 
-            // dgAllotmentClasses
+            // txtSearch
             // 
-            this.dgAllotmentClasses.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgAllotmentClasses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgAllotmentClasses.Location = new System.Drawing.Point(12, 54);
-            this.dgAllotmentClasses.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dgAllotmentClasses.Name = "dgAllotmentClasses";
-            this.dgAllotmentClasses.RowHeadersWidth = 51;
-            this.dgAllotmentClasses.RowTemplate.Height = 29;
-            this.dgAllotmentClasses.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgAllotmentClasses.Size = new System.Drawing.Size(675, 323);
-            this.dgAllotmentClasses.TabIndex = 3;
-            this.dgAllotmentClasses.SelectionChanged += new System.EventHandler(this.dgAllotmentClasses_SelectionChanged_1);
+            txtSearch.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new System.Drawing.Size(200, 42);
             // 
             // statusStrip1
             // 
-            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1,
-            this.lblRecordCount,
-            this.toolStripStatusLabel4,
-            this.toolStripStatusLabel2,
-            this.lblCreatedAt,
-            this.toolStripStatusLabel3,
-            this.lblUpdatedAt});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 379);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(699, 22);
-            this.statusStrip1.SizingGrip = false;
-            this.statusStrip1.TabIndex = 5;
-            this.statusStrip1.Text = "statusStrip1";
+            statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripStatusLabel1, lblRecordCount, toolStripStatusLabel4, toolStripStatusLabel2, lblCreatedAt, toolStripStatusLabel3, lblUpdatedAt });
+            statusStrip1.Location = new System.Drawing.Point(0, 345);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
+            statusStrip1.Size = new System.Drawing.Size(729, 22);
+            statusStrip1.SizingGrip = false;
+            statusStrip1.TabIndex = 5;
+            statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(52, 17);
-            this.toolStripStatusLabel1.Text = "Records:";
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new System.Drawing.Size(52, 17);
+            toolStripStatusLabel1.Text = "Records:";
             // 
             // lblRecordCount
             // 
-            this.lblRecordCount.Name = "lblRecordCount";
-            this.lblRecordCount.Size = new System.Drawing.Size(13, 17);
-            this.lblRecordCount.Text = "0";
+            lblRecordCount.Name = "lblRecordCount";
+            lblRecordCount.Size = new System.Drawing.Size(13, 17);
+            lblRecordCount.Text = "0";
             // 
             // toolStripStatusLabel4
             // 
-            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
-            this.toolStripStatusLabel4.Size = new System.Drawing.Size(489, 17);
-            this.toolStripStatusLabel4.Spring = true;
+            toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            toolStripStatusLabel4.Size = new System.Drawing.Size(519, 17);
+            toolStripStatusLabel4.Spring = true;
             // 
             // toolStripStatusLabel2
             // 
-            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(64, 17);
-            this.toolStripStatusLabel2.Text = "Created at:";
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new System.Drawing.Size(64, 17);
+            toolStripStatusLabel2.Text = "Created at:";
             // 
             // lblCreatedAt
             // 
-            this.lblCreatedAt.Name = "lblCreatedAt";
-            this.lblCreatedAt.Size = new System.Drawing.Size(0, 17);
+            lblCreatedAt.Name = "lblCreatedAt";
+            lblCreatedAt.Size = new System.Drawing.Size(0, 17);
             // 
             // toolStripStatusLabel3
             // 
-            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
-            this.toolStripStatusLabel3.Size = new System.Drawing.Size(68, 17);
-            this.toolStripStatusLabel3.Text = "Updated at:";
+            toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            toolStripStatusLabel3.Size = new System.Drawing.Size(68, 17);
+            toolStripStatusLabel3.Text = "Updated at:";
             // 
             // lblUpdatedAt
             // 
-            this.lblUpdatedAt.Name = "lblUpdatedAt";
-            this.lblUpdatedAt.Size = new System.Drawing.Size(0, 17);
+            lblUpdatedAt.Name = "lblUpdatedAt";
+            lblUpdatedAt.Size = new System.Drawing.Size(0, 17);
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(dgAllotmentClasses);
+            panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel1.Location = new System.Drawing.Point(0, 50);
+            panel1.Name = "panel1";
+            panel1.Padding = new System.Windows.Forms.Padding(4);
+            panel1.Size = new System.Drawing.Size(729, 295);
+            panel1.TabIndex = 6;
+            // 
+            // dgAllotmentClasses
+            // 
+            dgAllotmentClasses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgAllotmentClasses.Dock = System.Windows.Forms.DockStyle.Fill;
+            dgAllotmentClasses.Location = new System.Drawing.Point(4, 4);
+            dgAllotmentClasses.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            dgAllotmentClasses.Name = "dgAllotmentClasses";
+            dgAllotmentClasses.RowHeadersWidth = 51;
+            dgAllotmentClasses.RowTemplate.Height = 29;
+            dgAllotmentClasses.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            dgAllotmentClasses.Size = new System.Drawing.Size(721, 287);
+            dgAllotmentClasses.TabIndex = 4;
+            dgAllotmentClasses.SelectionChanged += dgAllotmentClasses_SelectionChanged;
             // 
             // frmAllotmentClasses
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(699, 401);
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.dgAllotmentClasses);
-            this.Controls.Add(this.statusStrip1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "frmAllotmentClasses";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Manage > Allotment Classes";
-            this.Load += new System.EventHandler(this.frmAllotmentClasses_Load);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgAllotmentClasses)).EndInit();
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            ClientSize = new System.Drawing.Size(729, 367);
+            Controls.Add(panel1);
+            Controls.Add(toolStrip1);
+            Controls.Add(statusStrip1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmAllotmentClasses";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Manage > Allotment Classes";
+            Load += frmAllotmentClasses_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgAllotmentClasses).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -209,7 +216,6 @@ namespace AccountingSystem.Views.Manage.AllotmentClasses
         private System.Windows.Forms.ToolStripButton btnAdd;
         private System.Windows.Forms.ToolStripButton btnEdit;
         private System.Windows.Forms.ToolStripButton btnDelete;
-        private System.Windows.Forms.DataGridView dgAllotmentClasses;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel lblRecordCount;
@@ -218,5 +224,8 @@ namespace AccountingSystem.Views.Manage.AllotmentClasses
         private System.Windows.Forms.ToolStripStatusLabel lblCreatedAt;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
         private System.Windows.Forms.ToolStripStatusLabel lblUpdatedAt;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgAllotmentClasses;
+        private System.Windows.Forms.ToolStripTextBox txtSearch;
     }
 }
--- AccountingSystem/Views/Manage/AllotmentClasses/frmAllotmentClasses.cs
+++ AccountingSystem/Views/Manage/AllotmentClasses/frmAllotmentClasses.cs
@@ -11,26 +11,25 @@ namespace AccountingSystem.Views.Manage.AllotmentClasses
         {
             InitializeComponent();
             Helper.LoadFormIcon(this);
+            Helper.DatagridFullRowSelectStyle(dgAllotmentClasses, true);
         }
 
         internal void LoadRecords()
         {
-            try
-            {
-                var allotmentClassesRepository = AccFactory.AllotmentClassesRepository();
-                var dtAllotmentClasses = allotmentClassesRepository.GetRecords();
-                HelperLoadRecords.AllotmentClassesDatagridView(dtAllotmentClasses, dgAllotmentClasses);
+            var allotmentClassesRepository = AccFactory.AllotmentClassesRepository();
+            var dtAllotmentClasses = allotmentClassesRepository.GetRecords();
+            HelperLoadRecords.AllotmentClassesDatagridView(dtAllotmentClasses, dgAllotmentClasses);
 
-                lblRecordCount.Text = allotmentClassesRepository.CountRecords().ToString();
-            }
-            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
+            lblRecordCount.Text = allotmentClassesRepository.CountRecords().ToString();
         }
 
         private void frmAllotmentClasses_Load(object sender, EventArgs e)
         {
-            Helper.DatagridEditableRowStyle(dgAllotmentClasses, true);
-            dgAllotmentClasses.ShowCellToolTips = false;
-            LoadRecords();
+            try
+            {
+                LoadRecords();
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -40,43 +39,48 @@ namespace AccountingSystem.Views.Manage.AllotmentClasses
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            int allotmentId = int.Parse(dgAllotmentClasses.SelectedCells[0].Value.ToString());
+            int rowIndex = dgAllotmentClasses.CurrentRow.Index;
+            int allotmentId = Convert.ToInt32(dgAllotmentClasses.Rows[rowIndex].Cells["id"].Value);
+
             _ = new frmAllotmentClassesEdit(this, allotmentId).ShowDialog();
+        }
+
+        private bool DeleteData()
+        {
+            int selectedRowsCount = dgAllotmentClasses.SelectedRows.Count;
+
+            if (selectedRowsCount < 1)
+                return false;
+
+            if (Helper.MessageBoxConfirmDelete(selectedRowsCount))
+            {
+                var allotmentClassesModelList = new List<AllotmentClassesModel>();
+                foreach (DataGridViewRow row in dgAllotmentClasses.SelectedRows)
+                {
+                    int allotmentId = Convert.ToInt16(row.Cells["id"].Value.ToString());
+                    allotmentClassesModelList.Add(new AllotmentClassesModel() { Id = allotmentId });
+                }
+
+                return AccFactory.AllotmentClassesRepository().Delete(allotmentClassesModelList);
+            }
+            return false;
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            int selectedRowsCount = dgAllotmentClasses.SelectedRows.Count;
             try
             {
-                if (selectedRowsCount > 0)
-                {
-                    if (Helper.MessageBoxConfirmDelete(selectedRowsCount))
-                    {
-                        var allotmentClassesModelList = new List<AllotmentClassesModel>();
-                        foreach (DataGridViewRow row in dgAllotmentClasses.SelectedRows)
-                        {
-                            int allotmentId = Convert.ToInt16(row.Cells[0].Value.ToString());
-                            allotmentClassesModelList.Add(new AllotmentClassesModel() { Id = allotmentId });
-                        }
-
-                        var allotmentClassesRepository = AccFactory.AllotmentClassesRepository();
-                        _ = allotmentClassesRepository.Delete(allotmentClassesModelList);
-                        LoadRecords();
-                    }
-                }
+                if (DeleteData())
+                    LoadRecords();
             }
-            catch (Exception ex)
-            {
-                Helper.MessageBoxError(ex.Message);
-            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
 
         private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
         }
 
-        private void dgAllotmentClasses_SelectionChanged_1(object sender, EventArgs e)
+        private void dgAllotmentClasses_SelectionChanged(object sender, EventArgs e)
         {
             byte[] columnIndexTimestamp = { 3, 4 };
             Helper.ShowRecordTimestamp(dgAllotmentClasses, columnIndexTimestamp, lblCreatedAt, lblUpdatedAt);
