4e4fd9d99250ee2f80420327942224b51af483df
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-06-22 09:26:28 +0800

    Created Model, Interface and Repository for Realignment

--- ACC.Data/AccFactory.cs
+++ ACC.Data/AccFactory.cs
@@ -71,13 +71,20 @@ namespace AccountingSystem
         public static ISubFPPRepository SubFPPRepository() => new SubFPPRepository(mySqlGenericCommandsLFS);
 
         //A part of Budget System
+
+        #region Budget Approprations
+
         public static IBudgetAppropriationsRepository BudgetAppropriationsRepository() => new BudgetAppropriationsRepository(mySqlGenericCommandsLFS, SupplementalAppropriationsRepository());
+
+        public static IRealignments RealignmentsRepository() => new RealignmentsRepository(mySqlGenericCommandsLFS);
+
+        public static ISupplementalAppropriationsRepository SupplementalAppropriationsRepository() => new SupplementalAppropriationsRepository(mySqlGenericCommandsLFS);
+
+        #endregion Budget Approprations
 
         public static IAllotmentReleaseRepository AllotmentReleaseRepository() => new AllotmentReleaseRepository(mySqlGenericCommandsLFS, AllotmentAccountRepository());
 
         public static IAllotmentAccountRepository AllotmentAccountRepository() => new AllotmentAccountRepository(mySqlGenericCommandsLFS);
-
-        public static ISupplementalAppropriationsRepository SupplementalAppropriationsRepository() => new SupplementalAppropriationsRepository(mySqlGenericCommandsLFS);
 
         public static IObligationRequestRepository ObligationRequestRepository() => new ObligationRequestRepository(mySqlGenericCommandsLFS, ObligationAccountRepository());
 
