7b62d64c7b6233524c346cd3617e19b87449d9bd
A R C H I E <px1998.ass@gmail.com>
2023-09-11 09:36:01 +0800

    Fixed bank account UI.

--- AccountingSystem/Views/Manage/BankAccounts/frmBankAccounts.Designer.cs
+++ AccountingSystem/Views/Manage/BankAccounts/frmBankAccounts.Designer.cs
@@ -34,26 +34,28 @@
             toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             toolStripStatusLabelRecordCount = new System.Windows.Forms.ToolStripStatusLabel();
             toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
-            toolStrip1 = new System.Windows.Forms.ToolStrip();
+            toolStrip = new System.Windows.Forms.ToolStrip();
             btnAdd = new System.Windows.Forms.ToolStripButton();
             btnEdit = new System.Windows.Forms.ToolStripButton();
             btnDelete = new System.Windows.Forms.ToolStripButton();
-            txtSearch = new System.Windows.Forms.ToolStripTextBox();
+            btnSearch = new System.Windows.Forms.Button();
+            txtSearch = new System.Windows.Forms.TextBox();
+            pbLoadRecords = new System.Windows.Forms.ProgressBar();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgBankAccounts).BeginInit();
             statusStrip1.SuspendLayout();
-            toolStrip1.SuspendLayout();
+            toolStrip.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.Controls.Add(dgBankAccounts);
             panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            panel1.Location = new System.Drawing.Point(0, 50);
+            panel1.Location = new System.Drawing.Point(0, 55);
             panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
             panel1.Padding = new System.Windows.Forms.Padding(4);
-            panel1.Size = new System.Drawing.Size(500, 279);
+            panel1.Size = new System.Drawing.Size(794, 320);
             panel1.TabIndex = 16;
             // 
             // dgBankAccounts
@@ -65,16 +67,16 @@
             dgBankAccounts.Name = "dgBankAccounts";
             dgBankAccounts.RowHeadersWidth = 51;
             dgBankAccounts.RowTemplate.Height = 29;
-            dgBankAccounts.Size = new System.Drawing.Size(492, 271);
+            dgBankAccounts.Size = new System.Drawing.Size(786, 312);
             dgBankAccounts.TabIndex = 4;
             dgBankAccounts.SelectionChanged += dgBankAccounts_SelectionChanged;
             // 
             // statusStrip1
             // 
             statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabelRecordCount, toolStripStatusLabel2 });
-            statusStrip1.Location = new System.Drawing.Point(0, 329);
+            statusStrip1.Location = new System.Drawing.Point(0, 375);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new System.Drawing.Size(500, 22);
+            statusStrip1.Size = new System.Drawing.Size(794, 22);
             statusStrip1.TabIndex = 17;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -93,20 +95,21 @@
             // toolStripStatusLabel2
             // 
             toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            toolStripStatusLabel2.Size = new System.Drawing.Size(417, 17);
+            toolStripStatusLabel2.Size = new System.Drawing.Size(711, 17);
             toolStripStatusLabel2.Spring = true;
             // 
-            // toolStrip1
+            // toolStrip
             // 
-            toolStrip1.BackColor = System.Drawing.SystemColors.Control;
-            toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-            toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { btnAdd, btnEdit, btnDelete, txtSearch });
-            toolStrip1.Location = new System.Drawing.Point(0, 0);
-            toolStrip1.Name = "toolStrip1";
-            toolStrip1.Padding = new System.Windows.Forms.Padding(4);
-            toolStrip1.Size = new System.Drawing.Size(500, 50);
-            toolStrip1.TabIndex = 18;
-            toolStrip1.Text = "toolStrip1";
+            toolStrip.BackColor = System.Drawing.SystemColors.Control;
+            toolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            toolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { btnAdd, btnEdit, btnDelete });
+            toolStrip.Location = new System.Drawing.Point(0, 0);
+            toolStrip.Name = "toolStrip";
+            toolStrip.Padding = new System.Windows.Forms.Padding(4);
+            toolStrip.Size = new System.Drawing.Size(794, 50);
+            toolStrip.TabIndex = 19;
+            toolStrip.Text = "toolStrip1";
             // 
             // btnAdd
             // 
@@ -114,24 +117,25 @@
             btnAdd.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new System.Drawing.Size(42, 39);
-            btnAdd.Text = "Add...";
+            btnAdd.Size = new System.Drawing.Size(33, 39);
+            btnAdd.Text = "Add";
             btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            btnAdd.Click += btnAdd_Click;
+            btnAdd.ToolTipText = "Add";
             // 
             // btnEdit
             // 
+            btnEdit.Enabled = false;
             btnEdit.Image = Properties.Resources.button_rounded_edit_20px;
             btnEdit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             btnEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
             btnEdit.Name = "btnEdit";
-            btnEdit.Size = new System.Drawing.Size(40, 39);
-            btnEdit.Text = "Edit...";
+            btnEdit.Size = new System.Drawing.Size(31, 39);
+            btnEdit.Text = "Edit";
             btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            btnEdit.Click += btnEdit_Click;
             // 
             // btnDelete
             // 
+            btnDelete.Enabled = false;
             btnDelete.Image = Properties.Resources.button_rounded_remove_20px;
             btnDelete.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             btnDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
@@ -139,24 +143,46 @@
             btnDelete.Size = new System.Drawing.Size(44, 39);
             btnDelete.Text = "Delete";
             btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            btnSearch.Location = new System.Drawing.Point(712, 16);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new System.Drawing.Size(75, 23);
+            btnSearch.TabIndex = 28;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
             // 
             // txtSearch
             // 
-            txtSearch.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            txtSearch.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
             txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            txtSearch.Location = new System.Drawing.Point(510, 16);
+            txtSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             txtSearch.Name = "txtSearch";
-            txtSearch.Size = new System.Drawing.Size(200, 42);
-            txtSearch.TextChanged += txtSearch_TextChanged;
+            txtSearch.Size = new System.Drawing.Size(200, 23);
+            txtSearch.TabIndex = 27;
+            // 
+            // pbLoadRecords
+            // 
+            pbLoadRecords.Dock = System.Windows.Forms.DockStyle.Top;
+            pbLoadRecords.Location = new System.Drawing.Point(0, 50);
+            pbLoadRecords.Name = "pbLoadRecords";
+            pbLoadRecords.Size = new System.Drawing.Size(794, 5);
+            pbLoadRecords.TabIndex = 29;
             // 
             // frmBankAccounts
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(500, 351);
+            ClientSize = new System.Drawing.Size(794, 397);
             Controls.Add(panel1);
+            Controls.Add(pbLoadRecords);
+            Controls.Add(btnSearch);
+            Controls.Add(txtSearch);
+            Controls.Add(toolStrip);
             Controls.Add(statusStrip1);
-            Controls.Add(toolStrip1);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             MinimizeBox = false;
@@ -169,8 +195,8 @@
             ((System.ComponentModel.ISupportInitialize)dgBankAccounts).EndInit();
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
-            toolStrip1.ResumeLayout(false);
-            toolStrip1.PerformLayout();
+            toolStrip.ResumeLayout(false);
+            toolStrip.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -188,10 +214,12 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelUpdatedAt;
         private System.Windows.Forms.DataGridView dgBankAccounts;
-        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip;
         private System.Windows.Forms.ToolStripButton btnAdd;
         private System.Windows.Forms.ToolStripButton btnEdit;
         private System.Windows.Forms.ToolStripButton btnDelete;
-        private System.Windows.Forms.ToolStripTextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ProgressBar pbLoadRecords;
     }
 }
