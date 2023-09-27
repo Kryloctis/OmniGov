7b79c011149a3ddd42da8be4d33524c2d7dad711
A R C H I E <px1998.ass@gmail.com>
2023-09-27 12:29:10 +0800

    Added form for marriage license payment.

---
+++ AccountingSystem/Views/Transactions/Payments/OtherPayments/MarriageLicense/frmMarriageLicense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Transactions.Payments.OtherPayments.MarriageLicense
+{
+    public partial class frmMarriageLicense : Form
+    {
+        public frmMarriageLicense()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMarriageLicense_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
