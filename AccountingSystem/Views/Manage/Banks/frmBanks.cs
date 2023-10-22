611994a8c644b66aa26418f5b734fae1c19e2505
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-10-22 14:58:57 +0800

    Fixed loading of record list banks mainform.

--- AccountingSystem/Views/Manage/Banks/frmBanks.cs
+++ AccountingSystem/Views/Manage/Banks/frmBanks.cs
@@ -2,6 +2,7 @@
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Windows.Forms;
 
@@ -16,73 +17,6 @@ namespace AccountingSystem.Views.Manage.Banks
             Helper.DatagridFullRowSelectStyle(dgBanks, true);
         }
 
-
-        private DataColumn[] BanksDataColumns()
-        {
-            var dataColumns = new DataColumn[]
-            {
-                new DataColumn("id", typeof(int)),
-                new DataColumn("bank_code", typeof(string)),
-                new DataColumn("bank_name", typeof(string)),
-                new DataColumn("bank_branch", typeof(string)),
-                new DataColumn("created_at", typeof(DateTime)),
-                new DataColumn("updated_at", typeof(DateTime)),
-            };
-
-            return dataColumns;
-        }
-
-        internal void LoadBanks()
-        {
-            DataTable dtBanks = new();
-            DataTable dtBankFromDB;
-            dtBanks.Columns.AddRange(BanksDataColumns());
-
-            var searchKey = txtSearch.Text.Trim();
-
-            if (!string.IsNullOrEmpty(searchKey))
-                dtBankFromDB = AccFactory.BanksRepository().GetRecordsBySearch(searchKey);
-            else
-                dtBankFromDB = AccFactory.BanksRepository().GetRecords();
-
-
-            int recordCount = dtBankFromDB.Rows.Count;
-            int rowCount = 0;
-
-            foreach (DataRow row in dtBankFromDB.Rows)
-            {
-                var newRow = dtBanks.NewRow();
-
-                int id = Convert.ToInt32(row["id"]);
-                string bankCode = row["bank_code"].ToString();
-                string bankName = row["bank_name"].ToString();
-                string bankBranch = row["bank_branch"].ToString();
-
-                newRow["id"] = id;
-                newRow["bank_code"] = bankCode;
-                newRow["bank_name"] = bankName;
-                newRow["bank_branch"] = bankBranch;
-
-                rowCount++;
-                int progressBarPercentage = (rowCount * 100) / recordCount;
-                backgroundWorker1.ReportProgress(progressBarPercentage);
-                dtBanks.Rows.Add(newRow);
-            }
-
-            HelperLoadRecords.BanksDatagridView(dtBanks, dgBanks);
-            lblRecordCount.Text = AccFactory.BanksRepository().CountRecords().ToString();
-        }
-
-        internal void LoadRecords()
-        {
-            if (!backgroundWorker1.IsBusy)
-            {
-                pbLoadRecords.Value = 0;
-                backgroundWorker1.RunWorkerAsync();
-            }
-        }
-
-
         private void frmBanks_Load(object sender, EventArgs e)
         {
             try
@@ -94,13 +28,22 @@ namespace AccountingSystem.Views.Manage.Banks
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            _ = new frmAddBanks(this).ShowDialog();
+            try
+            {
+                _ = new frmAddBanks(this).ShowDialog();
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            int bankId = Convert.ToInt32(dgBanks.SelectedRows[0].Cells["id"].Value);
-            _ = new frmEditBank(this, bankId).ShowDialog();
+            try
+            {
+                var index = dgBanks.CurrentRow.Index;
+                int bankId = Convert.ToInt32(dgBanks.Rows[index].Cells["id"].Value);
+                _ = new frmEditBank(this, bankId).ShowDialog();
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
 
         private bool DeleteRecords()
@@ -142,33 +85,134 @@ namespace AccountingSystem.Views.Manage.Banks
 
         private void dgBanks_SelectionChanged(object sender, EventArgs e)
         {
-            byte[] columnIndexTimestamp = { 4, 5 };
-            Helper.ShowRecordTimestamp(dgBanks, columnIndexTimestamp, lblCreatedAt, lblUpdatedAt);
-            Helper.EnableDisableToolStripButtons(dgBanks, btnEdit, btnDelete);
+            try
+            {
+                byte[] columnIndexTimestamp = { 4, 5 };
+                Helper.ShowRecordTimestamp(dgBanks, columnIndexTimestamp, lblCreatedAt, lblUpdatedAt);
+                Helper.EnableDisableToolStripButtons(dgBanks, btnEdit, btnDelete);
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
-
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            LoadRecords();
-        }
-
-        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
-        {
-            Invoke((MethodInvoker)delegate
+            try
             {
-                LoadBanks();
-            });
+                LoadRecords();
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
 
-        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        private DataColumn[] BanksDataColumns()
+        {
+            var dataColumns = new DataColumn[]
+            {
+                new DataColumn("id", typeof(int)),
+                new DataColumn("bank_code", typeof(string)),
+                new DataColumn("bank_name", typeof(string)),
+                new DataColumn("bank_branch", typeof(string)),
+                new DataColumn("created_at", typeof(string)),
+                new DataColumn("updated_at", typeof(string)),
+            };
+
+            return dataColumns;
+        }
+
+        private Dictionary<string, string> LoadBanksParameters()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            var searchKey = txtSearch.Text.Trim();
+            dictionary.Add("search_key", searchKey);
+
+            return dictionary;
+        }
+
+        internal void LoadRecords()
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                pbLoadRecords.Value = 0;
+                backgroundWorker1.RunWorkerAsync(LoadBanksParameters());
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (e.Argument is not Dictionary<string, string> dictParameters)
+                return;
+
+            var dataTable = new DataTable();
+            DataTable dtBankFromDB = AccFactory.BanksRepository().GetRecordsBySearch(dictParameters["search_key"]);
+            dataTable.Columns.AddRange(BanksDataColumns());
+
+            if (dtBankFromDB.Rows.Count < 1)
+            {
+                backgroundWorker1.ReportProgress(100);
+                e.Result = dataTable;
+                return;
+            }
+
+            int totalProgressCount = dtBankFromDB.Rows.Count;
+            int progressCount = 0;
+
+            foreach (DataRow row in dtBankFromDB.Rows)
+            {
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                var newRow = dataTable.NewRow();
+                int id = Convert.ToInt32(row["id"]);
+                string bankCode = row["bank_code"].ToString();
+                string bankName = row["bank_name"].ToString();
+                string bankBranch = row["bank_branch"].ToString();
+                string createdAt = row["created_at"].ToString();
+                string updatedAt = row["updated_at"].ToString();
+
+                newRow["id"] = id;
+                newRow["bank_code"] = bankCode;
+                newRow["bank_name"] = bankName;
+                newRow["bank_branch"] = bankBranch;
+                newRow["created_at"] = createdAt;
+                newRow["updated_at"] = updatedAt;
+
+                progressCount++;
+                dataTable.Rows.Add(newRow);
+
+                Helper.ProgressCounter(backgroundWorker1, totalProgressCount, progressCount);
+            }
+
+            e.Result = dataTable;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             pbLoadRecords.Value = e.ProgressPercentage;
         }
 
-        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            if (e.Cancelled)
+                return;
 
+            if (e.Result is not DataTable dataTable)
+                return;
+
+            HelperLoadRecords.BanksDatagridView(dataTable, dgBanks);
+            dgBanks.CurrentCell = dgBanks.FirstDisplayedCell;
+            lblRecordCount.Text = dgBanks.Rows.Count.ToString();
+        }
+
+        private void frmBanks_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                backgroundWorker1.CancelAsync();
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
     }
 }
