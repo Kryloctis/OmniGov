525596db01416849961edb3b06a0b14f71659847
A R C H I E <px1998.ass@gmail.com>
2023-11-08 10:34:00 +0800

    Created RDLC for notice of delinquency report.

--- AccountingSystem/dsLFS.cs
+++ AccountingSystem/dsLFS.cs
@@ -185,3 +185,71 @@ namespace AccountingSystem
 namespace AccountingSystem
 {
 }
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
