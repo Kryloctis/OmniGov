63dc10282b1e02e3983c9ecf3883abcd84986eed
A R C H I E <px1998.ass@gmail.com>
2023-11-29 14:08:38 +0800

    Added datatable for list of real property tax with warrant of levy.

--- AccountingSystem/dsLTOM.cs
+++ AccountingSystem/dsLTOM.cs
@@ -214,5 +214,41 @@ namespace AccountingSystem
 {
 }
 
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
+namespace AccountingSystem
+{
+}
+
 public partial class dsLTOM {
 }
