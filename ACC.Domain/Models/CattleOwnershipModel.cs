800c96266b7dccfd44b202245e48232224800591
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-12-10 04:41:13 +0800

    Reconstruct cattle ownership model.

--- ACC.Domain/Models/CattleOwnershipModel.cs
+++ ACC.Domain/Models/CattleOwnershipModel.cs
@@ -6,6 +6,13 @@ namespace ACC.Domain.Models
     {
         public int Id { get; set; }
         public PaymentCollectionsModel PaymentCollections { get; set; }
-        public regi
+        public RegistryModel Registry { get; set; }
+        public string CattleName { get; set; }
+        public string CattleSex { get; set; }
+        public string CattleAge { get; set; }
+        public string Description { get; set; }
+        public decimal CattlePrice { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
     }
 }
