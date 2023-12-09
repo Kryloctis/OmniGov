6fdc87cfa979e97b5ec8b4275633503e1f04d7f3
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-12-10 04:29:05 +0800

    Created registry model.

--- ACC.Domain/Models/CattleOwnershipModel.cs
+++ ACC.Domain/Models/CattleOwnershipModel.cs
@@ -4,18 +4,8 @@ namespace ACC.Domain.Models
 {
     public class CattleOwnershipModel
     {
-        public int ID { get; set; }
-        public string OwnerName { get; set; }
-        public int OwnerID { get; set; }
-        public int Tag { get; set; }
-        public string OwnerBarangay { get; set; }
-        public string OwnerMunicipality { get; set; }
-        public string OwnerProvince { get; set; }
-        public string CattleType { get; set; }
-        public string CattleSex { get; set; }
-        public int CattleAge { get; set; }
-        public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public int CreatedBy { get; set; }
+        public int Id { get; set; }
+        public PaymentCollectionsModel PaymentCollections { get; set; }
+        public regi
     }
 }
---
+++ ACC.Domain/Models/RegistryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACC.Domain.Models
+{
+    public class RegistryModel
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public string Nationality { get; set; }
+        public string Street { get; set; }
+        public string Municipality { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public string ContactInfo { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
