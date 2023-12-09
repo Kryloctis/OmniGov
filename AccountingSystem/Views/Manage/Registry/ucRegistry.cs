935bd69a40cc43ca7fd0fcad2dccee80a0506e72
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-12-10 04:10:28 +0800

    Added validation to ucRegistry.

--- AccountingSystem/Views/Manage/Registry/ucRegistry.cs
+++ AccountingSystem/Views/Manage/Registry/ucRegistry.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ACC.Data;
+using DocumentFormat.OpenXml.Office2010.PowerPoint;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,62 +19,111 @@ namespace AccountingSystem.Views.Manage.Registry
             InitializeComponent();
         }
 
+        internal string GetFormErrors()
+        {
+            var errors = new string[]
+            {
+                errorProvider1.GetError(txtFirstName),
+                errorProvider1.GetError(txtLastName),
+                errorProvider1.GetError(txtNationality),
+                errorProvider1.GetError(txtBarangay),
+                errorProvider1.GetError(txtMunicipality),
+                errorProvider1.GetError(txtProvince),
+                errorProvider1.GetError(txtCountry)
+            };
+
+            return AccFactory.CreateErrors(errors).GenerateErrorMessage();
+        }
+
+        private void ResetFields()
+        {
+            txtFirstName.Clear();
+            txtMiddleName.Clear();
+            txtLastName.Clear();
+            radMale.Checked = true;
+            txtNationality.Clear();
+            txtContactInfo.Clear();
+            txtStreet.Clear();
+            txtBarangay.Clear();
+            txtMunicipality.Clear();
+            txtProvince.Clear();
+            txtCountry.Clear();
+        }
+
+        internal void OnLoad(bool isEdit)
+        {
+        }
+
         #region Validations
 
         private void txtFirstName_Validating(object sender, CancelEventArgs e)
         {
+            e.Cancel = Helper.ShowErrorTextBoxEmpty(errorProvider1, txtFirstName, "First Name");
         }
 
         private void txtFirstName_Validated(object sender, EventArgs e)
         {
+            Helper.ClearErrorTextBox(errorProvider1, txtFirstName);
         }
 
         private void txtLastName_Validating(object sender, CancelEventArgs e)
         {
+            e.Cancel = Helper.ShowErrorTextBoxEmpty(errorProvider1, txtLastName, "Last Name");
         }
 
         private void txtLastName_Validated(object sender, EventArgs e)
         {
+            Helper.ClearErrorTextBox(errorProvider1, txtLastName);
         }
 
         private void txtNationality_Validating(object sender, CancelEventArgs e)
         {
+            Helper.ShowErrorTextBoxEmpty(errorProvider1, txtNationality, "Nationality");
         }
 
         private void txtNationality_Validated(object sender, EventArgs e)
         {
+            Helper.ClearErrorTextBox(errorProvider1, txtNationality);
         }
 
         private void txtBarangay_Validating(object sender, CancelEventArgs e)
         {
+            e.Cancel = Helper.ShowErrorTextBoxEmpty(errorProvider1, txtBarangay, "Barangay");
         }
 
         private void txtBarangay_Validated(object sender, EventArgs e)
         {
+            Helper.ClearErrorTextBox(errorProvider1, txtBarangay);
         }
 
         private void txtMunicipality_Validating(object sender, CancelEventArgs e)
         {
+            e.Cancel = Helper.ShowErrorTextBoxEmpty(errorProvider1, txtMunicipality, "Municipality");
         }
 
         private void txtMunicipality_Validated(object sender, EventArgs e)
         {
+            Helper.ClearErrorTextBox(errorProvider1, txtMunicipality);
         }
 
         private void txtProvince_Validating(object sender, CancelEventArgs e)
         {
+            e.Cancel = Helper.ShowErrorTextBoxEmpty(errorProvider1, txtProvince, "Province");
         }
 
         private void txtProvince_Validated(object sender, EventArgs e)
         {
+            Helper.ClearErrorTextBox(errorProvider1, txtProvince);
         }
 
         private void txtCountry_Validating(object sender, CancelEventArgs e)
         {
+            e.Cancel = Helper.ShowErrorTextBoxEmpty(errorProvider1, txtCountry, "Country");
         }
 
         private void txtCountry_Validated(object sender, EventArgs e)
         {
+            Helper.ClearErrorTextBox(errorProvider1, txtCountry);
         }
 
         #endregion Validations
