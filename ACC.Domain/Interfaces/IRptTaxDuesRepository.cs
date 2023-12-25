9c17dea24829a719b9eba3cd69805eb49c702ca5
Janrey Ubane <janrey.ubane1998@gmail.com>
2023-12-25 13:34:50 +0800

    Fixed RptPayment collections repositories.

--- ACC.Domain/Interfaces/IRptTaxDuesRepository.cs
+++ ACC.Domain/Interfaces/IRptTaxDuesRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ACC.Domain.Models;
+using System.Collections.Generic;
 using System.Data;
 
 namespace ACC.Domain.Interfaces
@@ -6,5 +7,7 @@ namespace ACC.Domain.Interfaces
     public interface IRptTaxDuesRepository : IAccRepository<RptTaxDuesModel>
     {
         DataTable GetViewRecordsByRptPaymentPostsId(int rptPaymentsId);
+
+        bool BulkInsert(List<RptTaxDuesModel> entityList);
     }
 }
