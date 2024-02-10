e48d7b667002235ac28fa6f0161fd35e468551eb
Janrey Ubane <janrey.ubane1998@gmail.com>
2024-02-10 23:25:50 +0800

    Create model for prev cattle ownership.

---
+++ ACC.Domain/Models/PrevCattleOwnershipModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACC.Domain.Models
+{
+    public class PrevCattleOwnershipModel
+    {
+        private int Id { get; set; }
+        private int PreviousCattleOwnershipId { get; set; }
+        private decimal CattlePrice { get; set; }
+        private DateTime TransferDate { get; set; }
+    }
+}
