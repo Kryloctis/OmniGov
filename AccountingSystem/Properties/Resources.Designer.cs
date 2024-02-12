e57ac8c9e4fe65373c479a9d50fa4402c36a8ad2
Janrey Ubane <janrey.ubane1998@gmail.com>
2024-02-12 15:41:15 +0800

    Created receipts AF51, AF52, AF53, and AF58 rdlc.

--- AccountingSystem/Properties/Resources.Designer.cs
+++ AccountingSystem/Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace AccountingSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap AF51 {
+            get {
+                object obj = ResourceManager.GetObject("AF51", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap archive_box_add_filled_14px {
             get {
                 object obj = ResourceManager.GetObject("archive-box-add-filled@14px", resourceCulture);
