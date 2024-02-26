7c2ec290eaad304069c7af782bbb4f17381f1c74
Janrey Ubane <janrey.ubane1998@gmail.com>
2024-02-27 00:35:36 +0800

    Deleted general collection payment model, interface, and repository.

--- ACC.Data/AccFactory.cs
+++ ACC.Data/AccFactory.cs
@@ -114,15 +114,13 @@ namespace ACC.Data
 
         #region RCD
 
+        public static IGeneralCollectionsRepository GeneralCollectionsRepository() => new GeneralCollectionsRepository(mySqlGenericCommandsLFS);
+
+        public static IGeneralCollectionsDepositsRepository GeneralCollectionsDepositsRepository() => new GeneralCollectionsDepositsRepository(mySqlGenericCommandsLFS);
+
         public static ICollectorReportRepository CollectorReportRepository() => new CollectorReportRepository(mySqlGenericCommandsLFS, CollectorReportPaymentsRepository());
 
         public static ICollectorReportPaymentsRepository CollectorReportPaymentsRepository() => new CollectorReportPaymentsRepository(mySqlGenericCommandsLFS);
-
-        public static IGeneralCollectionsRepository GeneralCollectionsRepository() => new GeneralCollectionsRepository(mySqlGenericCommandsLFS);
-
-        public static IGeneralCollectionsPaymentsRepository GeneralCollectionsPaymentsRepository() => new GeneralCollectionsPaymentsRepository(mySqlGenericCommandsLFS);
-
-        public static IGeneralCollectionsDepositsRepository GeneralCollectionsDepositsRepository() => new GeneralCollectionsDepositsRepository(mySqlGenericCommandsLFS);
 
         #endregion RCD
 
