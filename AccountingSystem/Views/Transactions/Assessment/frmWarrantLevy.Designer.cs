1cdb2efe294816066a74417e350dc3ed4feb4544
Janrey Ubane <janrey.ubane1998@gmail.com>
2024-05-24 04:54:24 +0800

    Created form for warrant of levy.

--- AccountingSystem/Views/Transactions/Assessment/frmWarrantLevy.Designer.cs
+++ AccountingSystem/Views/Transactions/Assessment/frmWarrantLevy.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AccountingSystem.Views.Transactions.Assessment
 {
-    partial class frmRptDelinquencyNotices
+    partial class frmWarrantLevy
     {
         /// <summary>
         /// Required designer variable.
@@ -28,6 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            statusStrip1 = new System.Windows.Forms.StatusStrip();
+            toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            lblRecordCount = new System.Windows.Forms.ToolStripStatusLabel();
+            toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            lblCreatedAt = new System.Windows.Forms.ToolStripStatusLabel();
+            toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            lblUpdatedAt = new System.Windows.Forms.ToolStripStatusLabel();
             tabControl1 = new System.Windows.Forms.TabControl();
             tabPageMain = new System.Windows.Forms.TabPage();
             panel1 = new System.Windows.Forms.Panel();
@@ -41,36 +50,86 @@
             btnAdd = new System.Windows.Forms.ToolStripButton();
             btnEdit = new System.Windows.Forms.ToolStripButton();
             btnDelete = new System.Windows.Forms.ToolStripButton();
-            statusStrip1 = new System.Windows.Forms.StatusStrip();
-            toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            lblRecordCount = new System.Windows.Forms.ToolStripStatusLabel();
-            toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
-            toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
-            lblCreatedAt = new System.Windows.Forms.ToolStripStatusLabel();
-            toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
-            toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
-            lblUpdatedAt = new System.Windows.Forms.ToolStripStatusLabel();
             tabPageForm = new System.Windows.Forms.TabPage();
             panel3 = new System.Windows.Forms.Panel();
-            panel4 = new System.Windows.Forms.Panel();
-            btnSave = new System.Windows.Forms.Button();
-            ucDelinquenyNotice1 = new ucDelinquenyNotice();
+            ucWarrantLevy1 = new ucWarrantLevy();
             toolStrip2 = new System.Windows.Forms.ToolStrip();
             toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             statusStrip2 = new System.Windows.Forms.StatusStrip();
             backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            panel4 = new System.Windows.Forms.Panel();
+            btnSave = new System.Windows.Forms.Button();
+            statusStrip1.SuspendLayout();
             tabControl1.SuspendLayout();
             tabPageMain.SuspendLayout();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             panel2.SuspendLayout();
             toolStrip1.SuspendLayout();
-            statusStrip1.SuspendLayout();
             tabPageForm.SuspendLayout();
             panel3.SuspendLayout();
-            panel4.SuspendLayout();
             toolStrip2.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripStatusLabel1, lblRecordCount, toolStripStatusLabel2, toolStripStatusLabel3, lblCreatedAt, toolStripStatusLabel5, toolStripStatusLabel6, lblUpdatedAt });
+            statusStrip1.Location = new System.Drawing.Point(0, 375);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new System.Drawing.Size(761, 22);
+            statusStrip1.TabIndex = 30;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new System.Drawing.Size(52, 17);
+            toolStripStatusLabel1.Text = "Records:";
+            // 
+            // lblRecordCount
+            // 
+            lblRecordCount.Name = "lblRecordCount";
+            lblRecordCount.Size = new System.Drawing.Size(13, 17);
+            lblRecordCount.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new System.Drawing.Size(505, 17);
+            toolStripStatusLabel2.Spring = true;
+            // 
+            // toolStripStatusLabel3
+            // 
+            toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            toolStripStatusLabel3.Size = new System.Drawing.Size(64, 17);
+            toolStripStatusLabel3.Text = "Created at:";
+            // 
+            // lblCreatedAt
+            // 
+            lblCreatedAt.Name = "lblCreatedAt";
+            lblCreatedAt.Size = new System.Drawing.Size(17, 17);
+            lblCreatedAt.Text = "--";
+            // 
+            // toolStripStatusLabel5
+            // 
+            toolStripStatusLabel5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            toolStripStatusLabel5.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            toolStripStatusLabel5.Size = new System.Drawing.Size(10, 17);
+            toolStripStatusLabel5.Text = "|";
+            // 
+            // toolStripStatusLabel6
+            // 
+            toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            toolStripStatusLabel6.Size = new System.Drawing.Size(68, 17);
+            toolStripStatusLabel6.Text = "Updated at:";
+            // 
+            // lblUpdatedAt
+            // 
+            lblUpdatedAt.Name = "lblUpdatedAt";
+            lblUpdatedAt.Size = new System.Drawing.Size(17, 17);
+            lblUpdatedAt.Text = "--";
             // 
             // tabControl1
             // 
@@ -82,8 +141,7 @@
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
             tabControl1.Size = new System.Drawing.Size(769, 428);
-            tabControl1.TabIndex = 0;
-            tabControl1.SelectedIndexChanged += tabControl1_SelectedIndexChanged;
+            tabControl1.TabIndex = 1;
             // 
             // tabPageMain
             // 
@@ -118,7 +176,6 @@
             dataGridView1.RowTemplate.Height = 25;
             dataGridView1.Size = new System.Drawing.Size(753, 297);
             dataGridView1.TabIndex = 1;
-            dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
             // 
             // progressBar1
             // 
@@ -204,65 +261,6 @@
             btnDelete.Text = "Delete";
             btnDelete.Click += btnDelete_Click;
             // 
-            // statusStrip1
-            // 
-            statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { toolStripStatusLabel1, lblRecordCount, toolStripStatusLabel2, toolStripStatusLabel3, lblCreatedAt, toolStripStatusLabel5, toolStripStatusLabel6, lblUpdatedAt });
-            statusStrip1.Location = new System.Drawing.Point(0, 375);
-            statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new System.Drawing.Size(761, 22);
-            statusStrip1.TabIndex = 30;
-            statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripStatusLabel1
-            // 
-            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new System.Drawing.Size(52, 17);
-            toolStripStatusLabel1.Text = "Records:";
-            // 
-            // lblRecordCount
-            // 
-            lblRecordCount.Name = "lblRecordCount";
-            lblRecordCount.Size = new System.Drawing.Size(13, 17);
-            lblRecordCount.Text = "0";
-            // 
-            // toolStripStatusLabel2
-            // 
-            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            toolStripStatusLabel2.Size = new System.Drawing.Size(505, 17);
-            toolStripStatusLabel2.Spring = true;
-            // 
-            // toolStripStatusLabel3
-            // 
-            toolStripStatusLabel3.Name = "toolStripStatusLabel3";
-            toolStripStatusLabel3.Size = new System.Drawing.Size(64, 17);
-            toolStripStatusLabel3.Text = "Created at:";
-            // 
-            // lblCreatedAt
-            // 
-            lblCreatedAt.Name = "lblCreatedAt";
-            lblCreatedAt.Size = new System.Drawing.Size(17, 17);
-            lblCreatedAt.Text = "--";
-            // 
-            // toolStripStatusLabel5
-            // 
-            toolStripStatusLabel5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            toolStripStatusLabel5.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            toolStripStatusLabel5.Name = "toolStripStatusLabel5";
-            toolStripStatusLabel5.Size = new System.Drawing.Size(10, 17);
-            toolStripStatusLabel5.Text = "|";
-            // 
-            // toolStripStatusLabel6
-            // 
-            toolStripStatusLabel6.Name = "toolStripStatusLabel6";
-            toolStripStatusLabel6.Size = new System.Drawing.Size(68, 17);
-            toolStripStatusLabel6.Text = "Updated at:";
-            // 
-            // lblUpdatedAt
-            // 
-            lblUpdatedAt.Name = "lblUpdatedAt";
-            lblUpdatedAt.Size = new System.Drawing.Size(17, 17);
-            lblUpdatedAt.Text = "--";
-            // 
             // tabPageForm
             // 
             tabPageForm.Controls.Add(panel3);
@@ -279,7 +277,7 @@
             // 
             panel3.AutoScroll = true;
             panel3.Controls.Add(panel4);
-            panel3.Controls.Add(ucDelinquenyNotice1);
+            panel3.Controls.Add(ucWarrantLevy1);
             panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             panel3.Location = new System.Drawing.Point(0, 35);
             panel3.Name = "panel3";
@@ -287,34 +285,13 @@
             panel3.Size = new System.Drawing.Size(761, 340);
             panel3.TabIndex = 29;
             // 
-            // panel4
+            // ucWarrantLevy1
             // 
-            panel4.Controls.Add(btnSave);
-            panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            panel4.Location = new System.Drawing.Point(4, 515);
-            panel4.Name = "panel4";
-            panel4.Padding = new System.Windows.Forms.Padding(4);
-            panel4.Size = new System.Drawing.Size(736, 50);
-            panel4.TabIndex = 36;
-            // 
-            // btnSave
-            // 
-            btnSave.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
-            btnSave.Location = new System.Drawing.Point(579, 7);
-            btnSave.Name = "btnSave";
-            btnSave.Size = new System.Drawing.Size(150, 23);
-            btnSave.TabIndex = 0;
-            btnSave.Text = "Save (Ctrl + S)";
-            btnSave.UseVisualStyleBackColor = true;
-            btnSave.Click += btnSave_Click;
-            // 
-            // ucDelinquenyNotice1
-            // 
-            ucDelinquenyNotice1.Dock = System.Windows.Forms.DockStyle.Top;
-            ucDelinquenyNotice1.Location = new System.Drawing.Point(4, 4);
-            ucDelinquenyNotice1.Name = "ucDelinquenyNotice1";
-            ucDelinquenyNotice1.Size = new System.Drawing.Size(736, 511);
-            ucDelinquenyNotice1.TabIndex = 35;
+            ucWarrantLevy1.Dock = System.Windows.Forms.DockStyle.Top;
+            ucWarrantLevy1.Location = new System.Drawing.Point(4, 4);
+            ucWarrantLevy1.Name = "ucWarrantLevy1";
+            ucWarrantLevy1.Size = new System.Drawing.Size(736, 496);
+            ucWarrantLevy1.TabIndex = 0;
             // 
             // toolStrip2
             // 
@@ -346,15 +323,27 @@
             statusStrip2.TabIndex = 30;
             statusStrip2.Text = "statusStrip2";
             // 
-            // backgroundWorker1
+            // panel4
             // 
-            backgroundWorker1.WorkerReportsProgress = true;
-            backgroundWorker1.WorkerSupportsCancellation = true;
-            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
-            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
-            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+            panel4.Controls.Add(btnSave);
+            panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            panel4.Location = new System.Drawing.Point(4, 500);
+            panel4.Name = "panel4";
+            panel4.Padding = new System.Windows.Forms.Padding(4);
+            panel4.Size = new System.Drawing.Size(736, 72);
+            panel4.TabIndex = 1;
             // 
-            // frmRptDelinquencyNotices
+            // btnSave
+            // 
+            btnSave.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            btnSave.Location = new System.Drawing.Point(579, 7);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new System.Drawing.Size(150, 23);
+            btnSave.TabIndex = 0;
+            btnSave.Text = "Save (Ctrl + S)";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // frmWarrantLevy
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -362,12 +351,14 @@
             Controls.Add(tabControl1);
             MinimizeBox = false;
             MinimumSize = new System.Drawing.Size(785, 467);
-            Name = "frmRptDelinquencyNotices";
+            Name = "frmWarrantLevy";
             ShowIcon = false;
             ShowInTaskbar = false;
             StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            Text = "Transactions > Assessment > Delinquency Notices";
-            Load += frnRptAssessment_Load;
+            Text = "Transactions > Assessment > Warrant of Levy";
+            Load += frmWarrantLevy_Load;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             tabControl1.ResumeLayout(false);
             tabPageMain.ResumeLayout(false);
             tabPageMain.PerformLayout();
@@ -376,49 +367,47 @@
             panel2.ResumeLayout(false);
             toolStrip1.ResumeLayout(false);
             toolStrip1.PerformLayout();
-            statusStrip1.ResumeLayout(false);
-            statusStrip1.PerformLayout();
             tabPageForm.ResumeLayout(false);
             tabPageForm.PerformLayout();
             panel3.ResumeLayout(false);
-            panel4.ResumeLayout(false);
             toolStrip2.ResumeLayout(false);
             toolStrip2.PerformLayout();
+            panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPageMain;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.ComboBox cmbxRowLimit;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton btnSearch;
-        private System.Windows.Forms.ToolStripTextBox txtSearch;
-        private System.Windows.Forms.ToolStripButton btnAdd;
-        private System.Windows.Forms.ToolStripButton btnEdit;
-        private System.Windows.Forms.ToolStripButton btnDelete;
         private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.TabPage tabPageForm;
-        private System.Windows.Forms.ToolStrip toolStrip2;
-        private System.Windows.Forms.ToolStripButton toolStripButton2;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel lblRecordCount;
-        private ucDelinquenyNotice ucDelinquenyNotice1;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.StatusStrip statusStrip2;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
         private System.Windows.Forms.ToolStripStatusLabel lblCreatedAt;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
         private System.Windows.Forms.ToolStripStatusLabel lblUpdatedAt;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageMain;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox cmbxRowLimit;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnSearch;
+        private System.Windows.Forms.ToolStripTextBox txtSearch;
+        private System.Windows.Forms.ToolStripButton btnAdd;
+        private System.Windows.Forms.ToolStripButton btnEdit;
+        private System.Windows.Forms.ToolStripButton btnDelete;
+        private System.Windows.Forms.TabPage tabPageForm;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.StatusStrip statusStrip2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ucWarrantLevy ucWarrantLevy1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnSave;
     }
 }
---
+++ AccountingSystem/Views/Transactions/Assessment/frmWarrantLevy.cs
@@ -0,0 +1,97 @@
+﻿using ACC.Data;
+using ACC.Domain.Models;
+using AccountingSystem.Views.Manage.AccountableForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountingSystem.Views.Transactions.Assessment
+{
+    public partial class frmWarrantLevy : Form
+    {
+        private bool isEdit;
+        private ucWarrantLevy ucWarrantLevy;
+
+        public frmWarrantLevy()
+        {
+            InitializeComponent();
+            ucWarrantLevy = ucWarrantLevy1;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                isEdit = false;
+                ucWarrantLevy.OnLoad(isEdit);
+                tabControl1.SelectedTab = tabPageForm;
+                btnSave.Text = "Save (Ctrl + S)";
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                isEdit = true;
+                int rowIndex = dataGridView1.CurrentRow.Index;
+                int warrantLevyId = Convert.ToInt32(dataGridView1.Rows[rowIndex].Cells["warrant_levy_id"].Value);
+                ucWarrantLevy.OnLoad(isEdit, warrantLevyId);
+                tabControl1.SelectedTab = tabPageForm;
+                btnSave.Text = "Update (Ctrl + S)";
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
+        }
+
+        private bool DeleteData(DataGridView dataGridView)
+        {
+            var selectedRows = dataGridView.SelectedRows;
+
+            if (Helper.MessageBoxConfirmDelete(selectedRows.Count))
+            {
+                var registryModels = new List<DelinquentNoticeModel>();
+
+                foreach (DataGridViewRow row in selectedRows)
+                    registryModels.Add(new DelinquentNoticeModel() { Id = Convert.ToInt32(row.Cells["delinquent_notice_id"].Value) });
+
+                return AccFactory.DelinquentNoticeRepository().Delete(registryModels);
+            }
+            return false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DeleteData(dataGridView1))
+                {
+                    Helper.MessageBoxSuccess($"{dataGridView1.SelectedRows.Count} record/s has been deleted.");
+                }
+            }
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbxRowLimit_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void frmWarrantLevy_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
