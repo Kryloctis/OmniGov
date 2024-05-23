47ea7a3775021e413603b4382a96f7defdc66cd9
Janrey Ubane <janrey.ubane1998@gmail.com>
2024-05-23 18:24:53 +0800

    Created model for delinquent notice.

---
+++ ACC.Domain/Models/DelinquentNoticeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ACC.Domain.Models
+{
+    public class DelinquentNoticeModel
+    {
+        public int Id { get; set; }
+        public int RealPropertiesId { get; set; }
+        public string NoticeType { get; set; }
+        public DateTime NoticeDate { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
