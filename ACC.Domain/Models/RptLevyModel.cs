810f4e1a1333d53cc6b489d456321fe649ce6307
Janrey Ubane <janrey.ubane1998@gmail.com>
2024-05-24 14:13:17 +0800

    Created model for rpt levy.

---
+++ ACC.Domain/Models/RptLevyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ACC.Domain.Models
+{
+    public class RptLevyModel
+    {
+        public int Id { get; set; }
+        public int RealPropertiesId { get; set; }
+        public DateTime DateTime { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
