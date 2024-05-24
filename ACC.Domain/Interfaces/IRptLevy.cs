eec9ff4b4a78942d13259ca7709daa0c4cd93436
Janrey Ubane <janrey.ubane1998@gmail.com>
2024-05-24 14:15:10 +0800

    Created interface for rpt levy.

---
+++ ACC.Domain/Interfaces/IRptLevy.cs
@@ -0,0 +1,12 @@
+﻿using ACC.Domain.Models;
+using System.Data;
+
+namespace ACC.Domain.Interfaces
+{
+    public interface IRptLevy : IAccRepository<RptLevyModel>
+    {
+        DataTable GetViewRecordsBySearch(int rowLimit, string searchKey);
+
+        DataTable GetViewRecordById(int Id);
+    }
+}
