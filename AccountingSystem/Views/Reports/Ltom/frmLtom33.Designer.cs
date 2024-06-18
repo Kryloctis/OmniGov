f67ddfd1c912b8867009c1ac93a3b41efe6398c7
A R C H I E <archiesorianosale1998@gmail.com>
2024-06-18 13:34:17 +0800

    Added report viewer for cancellation of warrant LTOM 33.

--- AccountingSystem/Views/Reports/Ltom/frmLtom33.Designer.cs
+++ AccountingSystem/Views/Reports/Ltom/frmLtom33.Designer.cs
@@ -28,21 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            splitContainer1 = new System.Windows.Forms.SplitContainer();
+            cmbxAuctionSchedule = new System.Windows.Forms.ComboBox();
+            label3 = new System.Windows.Forms.Label();
+            cmbxProperty = new System.Windows.Forms.ComboBox();
+            label1 = new System.Windows.Forms.Label();
+            btnRunReport = new System.Windows.Forms.Button();
+            panel3 = new System.Windows.Forms.Panel();
+            statusStrip1 = new System.Windows.Forms.StatusStrip();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            splitContainer1.Location = new System.Drawing.Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(cmbxAuctionSchedule);
+            splitContainer1.Panel1.Controls.Add(label3);
+            splitContainer1.Panel1.Controls.Add(cmbxProperty);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(btnRunReport);
+            splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(4);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(panel3);
+            splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(4);
+            splitContainer1.Size = new System.Drawing.Size(840, 406);
+            splitContainer1.SplitterDistance = 211;
+            splitContainer1.TabIndex = 14;
+            // 
+            // cmbxAuctionSchedule
+            // 
+            cmbxAuctionSchedule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            cmbxAuctionSchedule.FormattingEnabled = true;
+            cmbxAuctionSchedule.Location = new System.Drawing.Point(8, 26);
+            cmbxAuctionSchedule.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            cmbxAuctionSchedule.Name = "cmbxAuctionSchedule";
+            cmbxAuctionSchedule.Size = new System.Drawing.Size(200, 23);
+            cmbxAuctionSchedule.TabIndex = 34;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            label3.Location = new System.Drawing.Point(11, 13);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(106, 15);
+            label3.TabIndex = 33;
+            label3.Text = "Auction Schedule :";
+            // 
+            // cmbxProperty
+            // 
+            cmbxProperty.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            cmbxProperty.FormattingEnabled = true;
+            cmbxProperty.Location = new System.Drawing.Point(8, 72);
+            cmbxProperty.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            cmbxProperty.Name = "cmbxProperty";
+            cmbxProperty.Size = new System.Drawing.Size(200, 23);
+            cmbxProperty.TabIndex = 32;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            label1.Location = new System.Drawing.Point(11, 59);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(66, 15);
+            label1.TabIndex = 31;
+            label1.Text = "Properties :";
+            // 
+            // btnRunReport
+            // 
+            btnRunReport.Location = new System.Drawing.Point(8, 101);
+            btnRunReport.Name = "btnRunReport";
+            btnRunReport.Size = new System.Drawing.Size(200, 23);
+            btnRunReport.TabIndex = 30;
+            btnRunReport.Text = "Run Report";
+            btnRunReport.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel3.Location = new System.Drawing.Point(4, 4);
+            panel3.Name = "panel3";
+            panel3.Size = new System.Drawing.Size(617, 398);
+            panel3.TabIndex = 12;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new System.Drawing.Point(0, 406);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new System.Drawing.Size(840, 22);
+            statusStrip1.TabIndex = 15;
+            statusStrip1.Text = "statusStrip1";
             // 
             // frmLtom33
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(840, 428);
+            Controls.Add(splitContainer1);
+            Controls.Add(statusStrip1);
             Name = "frmLtom33";
             ShowIcon = false;
             ShowInTaskbar = false;
             StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             Text = "Reports > Treasury > Cancellation of Warrant of Levy (Local Assessor and Registrar of Deeds)";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox cmbxAuctionSchedule;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbxProperty;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRunReport;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
