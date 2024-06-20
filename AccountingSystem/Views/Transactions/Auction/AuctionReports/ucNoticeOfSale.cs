22398870610e811d50f2d5a8d1953443cd169b67
A R C H I E <archiesorianosale1998@gmail.com>
2024-06-21 05:26:23 +0800

    Changed date format of auction date in LTOM 24.

--- AccountingSystem/Views/Transactions/Auction/AuctionReports/ucNoticeOfSale.cs
+++ AccountingSystem/Views/Transactions/Auction/AuctionReports/ucNoticeOfSale.cs
@@ -168,14 +168,14 @@ namespace AccountingSystem.Views.Transactions.Auction
 
                 string lguName = Helper.LGUDetails()["lgu_name"];
                 string location = dtAuction["location"];
-                string date = dtAuction["start_date"];
+                string date = $"{Convert.ToDateTime(dtAuction["start_date"]):MMMM dd, yyyy} - {Convert.ToDateTime(dtAuction["end_date"]):MMMM dd, yyyy}";
 
                 var dictAuctionProperty = AccFactory.RptAuctionRepository().GetAuctionPropertiesByAuctionIdAndRptId(auctionId, propertyId);
 
                 var reportParameters = new ReportParameter[]
                 {
                     new ReportParameter("paramLGU", lguName),
-                    new ReportParameter("paramTimeOfAuction", date),
+                    new ReportParameter("paramDateOfAuction", date),
                     new ReportParameter("paramPlaceOfAuction", location),
                     new ReportParameter("paramDeclaredOwner", dictAuctionProperty["taxpayer_name"]),
                     new ReportParameter("paramCompleteAddress", dictAuctionProperty["taxpayer_address"]),
