ba841f6842678444ff0dc6105b88f911b431de2d
A R C H I E <archiesorianosale1998@gmail.com>
2024-06-24 00:40:16 +0800

    Transferred and changed LTOM 29 reportviewer from using usercontrol into form.

--- AccountingSystem/Views/Reports/Ltom/frmLtom29.cs
+++ AccountingSystem/Views/Reports/Ltom/frmLtom29.cs
@@ -1,6 +1,8 @@
 ﻿using ACC.Data;
 using ACC.Domain.Models;
-using AccountingSystem.Views.Transactions.Auction;
+using AccountingSystem.DataSets;
+using AccountingSystem.Views.Shared;
+using Microsoft.Reporting.WinForms;
 using System;
 using System.Data;
 using System.Windows.Forms;
@@ -10,13 +12,14 @@ namespace AccountingSystem.Views.Reports.Ltom
 
     public partial class frmLtom29 : Form
     {
-        private ucCertificateOfSale ucCertificateOfSale;
+        int auctionId;
+        int rptId;
 
         public frmLtom29()
         {
             InitializeComponent();
-            ucCertificateOfSale = ucCertificateOfSale1;
-
+            Helper.LoadFormIcon(this);
+            panel1.Controls.Add(reportViewer1);
         }
 
         private void ToogleRunButton(bool isGenerated)
@@ -29,17 +32,46 @@ namespace AccountingSystem.Views.Reports.Ltom
         {
             try
             {
-                ToogleRunButton(false);
-                int auctionId = Convert.ToInt32(cmbxAuctionSchedule.SelectedValue);
-                int rptId = Convert.ToInt32(cmbxProperty.SelectedValue);
-
                 if (cmbxAuctionSchedule.SelectedIndex == -1 || cmbxProperty.SelectedIndex == -1)
                     return;
 
-                ucCertificateOfSale.OnLoad(auctionId, rptId);
-                ToogleRunButton(true);
+                LoadReport();
             }
             catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
+
+        }
+
+        private void LoadReport()
+        {
+
+            if (!backgroundWorker1.IsBusy)
+            {
+                progressBar1.Value = 0;
+                ToogleRunButton(false);
+                auctionId = Convert.ToInt32(cmbxAuctionSchedule.SelectedValue);
+                rptId = Convert.ToInt32(cmbxProperty.SelectedValue);
+                backgroundWorker1.RunWorkerAsync((auctionId, rptId));
+            }
+        }
+
+        private (decimal basicPenalty, decimal sefPenalty) GetPenalties(DateTime transactionDate,
+                                                        (int assessmentYear, string compelteArpNo, int effectivityQuarter, int effectivityYear) currentAssmntParameters,
+                                                        decimal penaltyRate,
+                                                        decimal basicTaxDue,
+                                                        decimal sefTaxDue)
+        {
+            var dictPrevAssmnt = AccFactory.RptAssessmentPostsRepository().GetViewRecentAssessmentRecord(currentAssmntParameters.compelteArpNo, currentAssmntParameters.assessmentYear);
+
+            int? prevAssmntYear = null;
+
+            if (dictPrevAssmnt.Count > 1)
+                prevAssmntYear = Convert.ToInt32(dictPrevAssmnt["year"]);
+
+            int monthsDelinquent = RealPropertyTaxComputations.GetMonthsDelinquent(transactionDate, (currentAssmntParameters.assessmentYear, currentAssmntParameters.effectivityQuarter, currentAssmntParameters.effectivityYear), prevAssmntYear.HasValue ? prevAssmntYear : null);
+            decimal basicPenalty = RealPropertyTaxComputations.GetPenalty(penaltyRate, monthsDelinquent, basicTaxDue);
+            decimal sefPenalty = RealPropertyTaxComputations.GetPenalty(penaltyRate, monthsDelinquent, sefTaxDue);
+
+            return (basicPenalty, sefPenalty);
         }
 
         private void cmbxAuctionSchedule_SelectedIndexChanged(object sender, EventArgs e)
@@ -85,9 +117,130 @@ namespace AccountingSystem.Views.Reports.Ltom
             LoadProperties();
         }
 
-        private void ucCertificateOfSale1_Load(object sender, EventArgs e)
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            try
+            {
+
+                var parameters = ((int auctionId, int rptId))e.Argument;
+                var rptAuctionModel = new RptAuctionModel() { AuctionId = parameters.auctionId };
+
+                int progressCount = 0;
+                var dictRpt = AccFactory.RealPropertiesRepository().GetRecordByID(parameters.rptId);
+                string completeArp = dictRpt["complete_arp_no"].ToString();
+
+                var dtDelinquentProperty = AccFactory.RptAssessmentPostsRepository().GetViewDeliquentRecords();
+                var dtRptAuctionProperties = new dsTreasury.dtLtom29DataTable();
+                int totalProgressCount = dtDelinquentProperty.Rows.Count;
+
+
+                foreach (DataRow drDelinquentProperty in dtDelinquentProperty.Rows)
+                {
+                    string completeArpNo = drDelinquentProperty["complete_arp_no"].ToString();
+
+                    var newRow = dtRptAuctionProperties.NewRow();
+
+                    decimal assessedValue = Convert.ToDecimal(drDelinquentProperty["assessed_value"]);
+                    decimal basicRate = Convert.ToDecimal(drDelinquentProperty["basic_rate"]);
+                    decimal sefRate = Convert.ToDecimal(drDelinquentProperty["sef_rate"]);
+                    int assessmentYear = Convert.ToInt32(drDelinquentProperty["year"]);
+                    int effectivityQuarter = Convert.ToInt32(drDelinquentProperty["effectivity_quarterly"]);
+                    int effectivityYear = Convert.ToInt32(drDelinquentProperty["effectivity_year"]);
+                    decimal basicTaxDue = RealPropertyTaxComputations.GetBasicTaxDue(basicRate, assessedValue);
+                    decimal sefTaxDue = RealPropertyTaxComputations.GetSefTaxDue(sefRate, assessedValue);
+                    decimal penaltyRate = Convert.ToDecimal(drDelinquentProperty["penalty_rate"]);
+
+                    var penaltyParameters = (assessmentYear, completeArpNo, effectivityQuarter, effectivityYear);
+                    var penalties = GetPenalties(DateTime.Now, penaltyParameters, penaltyRate, basicTaxDue, sefTaxDue);
+                    var total = (penalties.basicPenalty + penalties.sefPenalty).ToString("N2");
+
+                    newRow["tax_year"] = assessmentYear;
+                    newRow["assessed_value"] = assessedValue;
+                    newRow["basic_tax"] = basicTaxDue;
+                    newRow["basic_penalty"] = penalties.basicPenalty;
+                    newRow["sef_tax"] = sefTaxDue;
+                    newRow["sef_penalty"] = penalties.sefPenalty;
+                    newRow["expenses_of_sale"] = 0.0;
+                    newRow["total_amount"] = total;
+
+                    progressCount++;
+                    Helper.ProgressCounter(backgroundWorker1, totalProgressCount, progressCount);
+
+                    if (completeArp == completeArpNo)
+                        dtRptAuctionProperties.Rows.Add(newRow);
+                    else
+                        continue;
+                }
+
+                e.Result = dtRptAuctionProperties;
+
+            }
+
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
         {
 
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            try
+            {
+                if (e.Cancelled)
+                    return;
+                if (e.Result is not DataTable dataTable)
+                    return;
+
+                if (dataTable.Rows.Count < 1)
+                    progressBar1.Value = 100;
+
+                var report = reportViewer1.LocalReport;
+                report.ReportPath = $"{Application.StartupPath}Reports\\LTOM\\Ltom29CertificateOfSale.rdlc";
+                report.DataSources.Clear();
+
+                string lguName = Helper.LGUDetails()["lgu_name"];
+
+
+                var dictAuctionProperty = AccFactory.RptAuctionRepository().GetAuctionPropertiesByAuctionIdAndRptId(auctionId, rptId);
+                string date = $"{Convert.ToDateTime(dictAuctionProperty["start_date"]):MMMM dd, yyyy} - {Convert.ToDateTime(dictAuctionProperty["end_date"]):MMMM dd, yyyy}";
+
+                var dictBid = AccFactory.BidRepository().GetHighestBidderByAuctionIdAndRptId(auctionId, rptId);
+
+                var reportParameters = new ReportParameter[]
+                {
+                    new ReportParameter("paramLGU", lguName),
+                    new ReportParameter("paramSignatoryTitle", string.Empty),
+                    new ReportParameter("paramSignatory", string.Empty),
+                    new ReportParameter("paramDeclaredOwner", dictAuctionProperty["taxpayer_name"]),
+                    new ReportParameter("paramTaxDec", dictAuctionProperty["complete_arp_no"]),
+                    new ReportParameter("paramARPNo", dictAuctionProperty["complete_arp_no"]),
+                    new ReportParameter("paramTCT", string.Empty),
+                    new ReportParameter("paramPIN", string.Empty),
+                    new ReportParameter("paramPropertyLocation", dictAuctionProperty["location"]),
+                    new ReportParameter("paramKindOfProperty", dictAuctionProperty["property_kind"]),
+
+                    new ReportParameter("paramDateOfPublicAuction", date),
+                    new ReportParameter("paramBuyer",  dictBid["name"]),
+                    new ReportParameter("paramBuyerAddress",  dictBid["address"]),
+                    new ReportParameter("paramSoldPrice",  dictBid["bid_amount"]),
+                    new ReportParameter("paramOfficialReceiptNo",  dictBid["receipt_no"]),
+                    new ReportParameter("paramSoldDate",  dictBid["date"]),
+                };
+
+                report.DataSources.Add(new ReportDataSource("dtLtom29", dataTable));
+                report.SetParameters(reportParameters);
+
+                reportViewer1.SetDisplayMode(DisplayMode.PrintLayout);
+                reportViewer1.ZoomMode = ZoomMode.FullPage;
+                reportViewer1.RefreshReport();
+
+                ToogleRunButton(true);
+            }
+
+            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
         }
     }
 }
