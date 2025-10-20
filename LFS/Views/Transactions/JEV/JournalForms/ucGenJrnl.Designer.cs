13d8e8dd6e6c9d38b696b77f3637b17a1f3adf7b
Janrey Ubane <47897595+Hyubane25@users.noreply.github.com>
2025-10-20 10:43:36 +0800

    Create user separated user control for "General Journal" JEV

---
+++ LFS/Views/Transactions/JEV/JournalForms/ucGenJrnl.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace LFS.Views.Transactions.JEV
+{
+    partial class ucGenJrnl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            textBox3 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            label5 = new System.Windows.Forms.Label();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label6 = new System.Windows.Forms.Label();
+            textBox5 = new System.Windows.Forms.TextBox();
+            label7 = new System.Windows.Forms.Label();
+            textBox6 = new System.Windows.Forms.TextBox();
+            errorProvider1 = new System.Windows.Forms.ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(22, 27);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(46, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Ref No.";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(97, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(229, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(22, 56);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(62, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Check No.";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(97, 52);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(229, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(22, 85);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(38, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Payee";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(97, 81);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(229, 23);
+            textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(22, 114);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(61, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Date Entry";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new System.Drawing.Point(97, 110);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new System.Drawing.Size(229, 23);
+            dateTimePicker1.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(22, 143);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(45, 15);
+            label5.TabIndex = 0;
+            label5.Text = "OR No.";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new System.Drawing.Point(97, 139);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(229, 23);
+            textBox4.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(22, 172);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(44, 15);
+            label6.TabIndex = 0;
+            label6.Text = "DV No.";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new System.Drawing.Point(97, 168);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(229, 23);
+            textBox5.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(22, 201);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(69, 15);
+            label7.TabIndex = 0;
+            label7.Text = "Explanation";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new System.Drawing.Point(97, 197);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            textBox6.Size = new System.Drawing.Size(229, 50);
+            textBox6.TabIndex = 1;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            errorProvider1.ContainerControl = this;
+            // 
+            // ucGenJrnl
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "ucGenJrnl";
+            Padding = new System.Windows.Forms.Padding(20);
+            Size = new System.Drawing.Size(349, 263);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
