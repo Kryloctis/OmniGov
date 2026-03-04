215cefeaa50a72939939862186a7dbe5f6951507
Janrey Ubane <47897595+Hyubane25@users.noreply.github.com>
2026-03-04 23:02:40 +0800

    refactor(CashTickets): reorganize code for better structure
    - reorganized event handlers in frmCashTickets for clarity
    - moved LoadCashTickets call to frmCashTickets_Load event
    - rearranged code in frmCashTicketEdit for better readability
    - updated using statements for consistency
    - restructured ucCashTickets for improved maintainability

--- src/Apps/OmniGov.App/Views/Manage/CashTickets/frmCashTicketEdit.cs
+++ src/Apps/OmniGov.App/Views/Manage/CashTickets/frmCashTicketEdit.cs
@@ -1,12 +1,12 @@
-using OmniGov.App.Helpers;
+﻿using OmniGov.App.Helpers;
 using OmniGov.Treasury.Data.Factories;
 
 namespace OmniGov.App.Views.Manage.CashTickets
 {
     public partial class frmCashTicketEdit : Form
     {
-        private readonly frmCashTickets frmCashTickets;
         private readonly int cashTicketId;
+        private readonly frmCashTickets frmCashTickets;
         private readonly ucCashTickets uc;
 
         public frmCashTicketEdit(frmCashTickets frmCashTickets, int cashTicketId)
@@ -17,11 +17,6 @@ namespace OmniGov.App.Views.Manage.CashTickets
             uc = ucCashTickets1;
         }
 
-        private void FrmCashTicketEdit_Load(object sender, EventArgs e)
-        {
-            uc.OnLoad(true, cashTicketId);
-        }
-
         private void btnUpdate_Click(object sender, EventArgs e)
         {
             if (UpdateData())
@@ -30,6 +25,11 @@ namespace OmniGov.App.Views.Manage.CashTickets
                 frmCashTickets.LoadCashTickets();
                 Close();
             }
+        }
+
+        private void FrmCashTicketEdit_Load(object sender, EventArgs e)
+        {
+            uc.OnLoad(true, cashTicketId);
         }
 
         private bool UpdateData()
