558348ed3e6d8515b7a59166ab22bb3cff5884d6
Janrey Ubane <47897595+Hyubane25@users.noreply.github.com>
2026-03-05 09:18:31 +0800

    refactor(FunctionalClassificationService): improve form functionality
    - updated frmFunctionalClassificationServiceAdd and frmFunctionalClassificationServiceEdit
      to handle save and edit operations more efficiently
    - removed redundant try-catch block in btnSave_Click event handler
    - improved code organization and readability
    - added LoadSelectedRecord method to load selected record data
    - modified SaveData method to validate user input before saving data

--- src/Apps/OmniGov.App/Views/Manage/FunctionProgramProject/FunctionalClassificationService/frmFunctionalClassificationServiceAdd.cs
+++ src/Apps/OmniGov.App/Views/Manage/FunctionProgramProject/FunctionalClassificationService/frmFunctionalClassificationServiceAdd.cs
@@ -1,60 +1,72 @@
-using OmniGov.App.Helpers;
+﻿using OmniGov.App.Helpers;
+
 using OmniGov.Core.Entities;
+
 using OmniGov.Core.Factories;
 
 namespace OmniGov.App.Views.Manage.FunctionProgramProject.FunctionalClassificationService
+
 {
     public partial class frmFunctionalClassificationServiceAdd : Form
+
     {
         private frmFunctionProgramProject _frmFunctionProgramProject;
 
         private ucFunctionalClassificationServices uc;
 
         public frmFunctionalClassificationServiceAdd(frmFunctionProgramProject frmFunctionProgramProject)
+
         {
             InitializeComponent();
+
             Helper.LoadFormIcon(this);
+
             uc = ucFunctonalClassificationServices1;
+
             _frmFunctionProgramProject = frmFunctionProgramProject;
         }
 
+        private void btnSave_Click(object sender, EventArgs e)
+
+        {
+            if (SaveData())
+            {
+                Helper.MessageBoxSuccess("Functional Classification Service has been saved.");
+                _frmFunctionProgramProject.LoadServiceNameComboBox();
+                _frmFunctionProgramProject.LoadFunctionClassificationServices();
+                Helper.DatagridViewRecordFinder(_frmFunctionProgramProject.dgFuntionalClassificationServices, "service_name", uc.txtName.Text);
+                uc.ResetForm();
+            }
+        }
+
         private bool SaveData()
+
         {
             // if error occurs, show messagebox error
+
             if (!uc.ValidateChildren())
+
             {
                 Helper.MessageBoxError(uc.GetFormErrors());
+
                 return false;
             }
 
             // proceed to insert
+
             int functionClassificationsId = Convert.ToInt32(uc.cmbSectorName.SelectedValue);
+
             string serviceName = uc.txtName.Text.Trim();
 
             var functionalClassificationServiceModel = new FunctionalClassificationServiceModel()
+
             {
                 functionalClassificationId = functionClassificationsId,
+
                 ServiceName = serviceName
             };
 
             return Factory.FunctionalClassificationServiceRepository().Insert(functionalClassificationServiceModel);
         }
-
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (SaveData())
-                {
-                    Helper.MessageBoxSuccess("Functional Classification Service has been saved.");
-                    _frmFunctionProgramProject.LoadServiceNameComboBox();
-                    _frmFunctionProgramProject.LoadFunctionClassificationServices();
-                    Helper.DatagridViewRecordFinder(_frmFunctionProgramProject.dgFuntionalClassificationServices, "service_name", uc.txtName.Text);
-                    uc.ResetForm();
-                }
-            }
-            catch (Exception ex) { Helper.MessageBoxError(ex.Message); }
-        }
     }
 }
-
